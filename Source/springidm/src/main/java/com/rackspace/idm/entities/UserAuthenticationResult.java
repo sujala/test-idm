0200675902db36ac1039723659872ab6a295e848
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-01-12 17:57:23 +0000

    IDM-55: Adding new AuthenticationResult objects

---
+++ Source/springidm/src/main/java/com/rackspace/idm/entities/UserAuthenticationResult.java
@@ -0,0 +1,50 @@
+package com.rackspace.idm.entities;
+
+public class UserAuthenticationResult extends AuthenticationResult {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        UserAuthenticationResult other = (UserAuthenticationResult) obj;
+        if (user == null) {
+            if (other.user != null) {
+                return false;
+            }
+        } else if (!user.equals(other.user)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAuthenticationResult [user=" + user + ", authenticated="
+            + authenticated + "]";
+    }
+}
