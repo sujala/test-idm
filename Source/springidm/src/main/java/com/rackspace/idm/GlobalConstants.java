77279c33d15c6579e437deb011af23175b79d579
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-02-11 06:29:48 +0000

    IDM-131: Removing References to GlobalConstants INUM_PREFIX

--- Source/springidm/src/main/java/com/rackspace/idm/GlobalConstants.java
+++ Source/springidm/src/main/java/com/rackspace/idm/GlobalConstants.java
@@ -9,8 +9,6 @@ public final class GlobalConstants {
     public static final String NO_REPLY_EMAIL = "no-reply@idm.rackspace.com";
     public static final String API_NAMESPACE_LOCATION = "http://docs.rackspacecloud.com/idm/api/v1.0";
 
-    public static final String INUM_PREFIX = "inum=";
-
     public static final String ATTR_SOFT_DELETED = "softDeleted";
 
     public static final String USER_PREFERRED_LANG_DEFAULT = "en_US";
