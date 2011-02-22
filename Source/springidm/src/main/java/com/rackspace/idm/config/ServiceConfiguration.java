7c5fda1f94318368dfc351e2a782c542b84bfee0
john.eo <john.eo@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-02-22 19:36:25 +0000

    IDM-107: Reformatting after merge

--- Source/springidm/src/main/java/com/rackspace/idm/config/ServiceConfiguration.java
+++ Source/springidm/src/main/java/com/rackspace/idm/config/ServiceConfiguration.java
@@ -1,16 +1,10 @@
 package com.rackspace.idm.config;
 
-import com.rackspace.idm.dao.*;
-import com.rackspace.idm.entities.AccessToken;
-import com.rackspace.idm.entities.EmailSettings;
-import com.rackspace.idm.entities.RefreshTokenDefaultAttributes;
-import com.rackspace.idm.oauth.DefaultOAuthService;
-import com.rackspace.idm.oauth.OAuthService;
-import com.rackspace.idm.services.*;
-import com.rackspace.idm.util.AuthHeaderHelper;
-import com.rackspace.idm.util.PingableService;
-import com.rackspace.idm.validation.InputValidator;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
 import net.spy.memcached.MemcachedClient;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -20,8 +14,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
+import com.rackspace.idm.dao.AccessTokenDao;
+import com.rackspace.idm.dao.AuthDao;
+import com.rackspace.idm.dao.ClientDao;
+import com.rackspace.idm.dao.CustomerDao;
+import com.rackspace.idm.dao.EndpointDao;
+import com.rackspace.idm.dao.RefreshTokenDao;
+import com.rackspace.idm.dao.RoleDao;
+import com.rackspace.idm.dao.UserDao;
+import com.rackspace.idm.dao.XdcAccessTokenDao;
+import com.rackspace.idm.entities.EmailSettings;
+import com.rackspace.idm.entities.RefreshTokenDefaultAttributes;
+import com.rackspace.idm.oauth.DefaultOAuthService;
+import com.rackspace.idm.oauth.OAuthService;
+import com.rackspace.idm.services.AccessTokenService;
+import com.rackspace.idm.services.AuthorizationService;
+import com.rackspace.idm.services.ClientService;
+import com.rackspace.idm.services.CustomerService;
+import com.rackspace.idm.services.DefaultAccessTokenService;
+import com.rackspace.idm.services.DefaultAuthorizationService;
+import com.rackspace.idm.services.DefaultClientService;
+import com.rackspace.idm.services.DefaultCustomerService;
+import com.rackspace.idm.services.DefaultEmailService;
+import com.rackspace.idm.services.DefaultEndpointService;
+import com.rackspace.idm.services.DefaultPasswordComplexityService;
+import com.rackspace.idm.services.DefaultRefreshTokenService;
+import com.rackspace.idm.services.DefaultRoleService;
+import com.rackspace.idm.services.DefaultUserService;
+import com.rackspace.idm.services.EmailService;
+import com.rackspace.idm.services.EndpointService;
+import com.rackspace.idm.services.HealthMonitoringService;
+import com.rackspace.idm.services.PasswordComplexityService;
+import com.rackspace.idm.services.RefreshTokenService;
+import com.rackspace.idm.services.RoleService;
+import com.rackspace.idm.services.UserService;
+import com.rackspace.idm.util.AuthHeaderHelper;
+import com.rackspace.idm.util.PingableService;
+import com.rackspace.idm.validation.InputValidator;
 
 /**
  * @author john.eo <br/>
@@ -82,10 +111,9 @@ public class ServiceConfiguration {
 
     @Bean
     public AccessTokenService tokenService() {
-        Logger logger = LoggerFactory
-            .getLogger(DefaultAccessTokenService.class);
-        return new DefaultAccessTokenService(accessTokenDao, clientDao,
-            userService(), xdcTokenDao, authHeaderHelper(), config, logger);
+        Logger logger = LoggerFactory.getLogger(DefaultAccessTokenService.class);
+        return new DefaultAccessTokenService(accessTokenDao, clientDao, userService(), xdcTokenDao,
+            authHeaderHelper(), config, logger);
     }
 
     @Bean
@@ -101,8 +129,7 @@ public class ServiceConfiguration {
     @Bean
     public ClientService clientService() {
         Logger logger = LoggerFactory.getLogger(DefaultClientService.class);
-        return new DefaultClientService(clientDao, customerDao, userRepo,
-            logger);
+        return new DefaultClientService(clientDao, customerDao, userRepo, logger);
     }
 
     @Bean
@@ -114,8 +141,7 @@ public class ServiceConfiguration {
     @Bean
     public CustomerService customerService() {
         Logger logger = LoggerFactory.getLogger(DefaultCustomerService.class);
-        return new DefaultCustomerService(clientDao, customerDao, userRepo,
-            logger);
+        return new DefaultCustomerService(clientDao, customerDao, userRepo, logger);
     }
 
     @Bean
@@ -128,8 +154,7 @@ public class ServiceConfiguration {
             config = new PropertiesConfiguration(propsFileLoc);
         } catch (ConfigurationException e) {
             e.printStackTrace();
-            throw new RuntimeException(
-                "Error encountered when loading the token config file.");
+            throw new RuntimeException("Error encountered when loading the token config file.");
         }
 
         int smtpPort = config.getInt("smtpPort");
@@ -140,40 +165,35 @@ public class ServiceConfiguration {
         boolean useSSL = config.getBoolean("SSL");
         boolean useTSL = config.getBoolean("TLS");
 
-        EmailSettings emailSettings = new EmailSettings(smtpPort, smtpHost,
-            smtpUsername, smtpPassword, debug, useSSL, useTSL);
+        EmailSettings emailSettings = new EmailSettings(smtpPort, smtpHost, smtpUsername, smtpPassword,
+            debug, useSSL, useTSL);
 
         return new DefaultEmailService(emailSettings);
     }
 
     @Bean
     public InputValidator inputValidator() {
-        Validator validator = Validation.buildDefaultValidatorFactory()
-            .getValidator();
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
         return new InputValidator(validator);
     }
 
     @Bean
     public PasswordComplexityService passwordComplexityService() {
-        Logger logger = LoggerFactory
-            .getLogger(DefaultPasswordComplexityService.class);
+        Logger logger = LoggerFactory.getLogger(DefaultPasswordComplexityService.class);
         return new DefaultPasswordComplexityService(logger);
     }
 
     @Bean
     public RefreshTokenService refreshTokenService() {
-        int defaultTokenExpirationSeconds = config
-            .getInt("token.refreshTokenExpirationSeconds");
+        int defaultTokenExpirationSeconds = config.getInt("token.refreshTokenExpirationSeconds");
         String dataCenterPrefix = config.getString("token.dataCenterPrefix");
 
         RefreshTokenDefaultAttributes defaultAttributes = new RefreshTokenDefaultAttributes(
             defaultTokenExpirationSeconds, dataCenterPrefix);
 
-        Logger logger = LoggerFactory
-            .getLogger(DefaultRefreshTokenService.class);
+        Logger logger = LoggerFactory.getLogger(DefaultRefreshTokenService.class);
 
-        return new DefaultRefreshTokenService(defaultAttributes,
-            refreshTokenDao, logger);
+        return new DefaultRefreshTokenService(defaultAttributes, refreshTokenDao, logger);
     }
 
     @Bean
@@ -185,10 +205,9 @@ public class ServiceConfiguration {
     @Bean
     public UserService userService() {
         Logger logger = LoggerFactory.getLogger(DefaultUserService.class);
-        boolean isTrustedServer = config.getBoolean("ldap.server.trusted",
-            false);
-        return new DefaultUserService(userRepo, authDao, customerDao,
-            emailService(), clientService(), isTrustedServer, logger);
+        boolean isTrustedServer = config.getBoolean("ldap.server.trusted", false);
+        return new DefaultUserService(userRepo, authDao, customerDao, emailService(), clientService(),
+            isTrustedServer, logger);
     }
 
     @Bean
@@ -200,9 +219,7 @@ public class ServiceConfiguration {
 
     @Bean
     public AuthorizationService authorizationService() {
-        Logger logger = LoggerFactory
-            .getLogger(DefaultAuthorizationService.class);
-        return new DefaultAuthorizationService(clientDao, memcached, config,
-            logger);
+        Logger logger = LoggerFactory.getLogger(DefaultAuthorizationService.class);
+        return new DefaultAuthorizationService(clientDao, memcached, config, logger);
     }
 }
