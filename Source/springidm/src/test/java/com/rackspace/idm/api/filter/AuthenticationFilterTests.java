f652d47eee21fd1eeb41c8c3bb6eca6503d2f647
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-03-01 16:30:51 +0000

    IDM-244: Moving stuff around

--- Source/springidm/src/test/java/com/rackspace/idm/api/filter/AuthenticationFilterTests.java
+++ Source/springidm/src/test/java/com/rackspace/idm/api/filter/AuthenticationFilterTests.java
@@ -1,5 +1,6 @@
-package com.rackspace.idm.interceptors;
+package com.rackspace.idm.api.filter;
 
+import com.rackspace.idm.api.filter.AuthenticationFilter;
 import com.rackspace.idm.exceptions.NotAuthenticatedException;
 import com.rackspace.idm.oauth.OAuthService;
 import com.rackspace.idm.services.AccessTokenService;
