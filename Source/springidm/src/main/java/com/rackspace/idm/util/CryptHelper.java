aa85a9ad7230b95029f1ae705eeb58cdede77db2
brad.brown <brad.brown@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-03-10 22:22:03 +0000

    IDM-239: Use weaker key size for export

--- Source/springidm/src/main/java/com/rackspace/idm/util/CryptHelper.java
+++ Source/springidm/src/main/java/com/rackspace/idm/util/CryptHelper.java
@@ -12,7 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CryptHelper {
-	private static final String ALOGRITHM = "PBEWithMD5AndTripleDES";
+	private static final String ALOGRITHM = "PBEWithMD5AndDES";
 
 	Logger logger = LoggerFactory.getLogger(CryptHelper.class);
 
