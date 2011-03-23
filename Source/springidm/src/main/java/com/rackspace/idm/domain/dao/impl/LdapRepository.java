fb6fee0cc04c145d1cc3b6f9840bbebf110d7b1f
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-03-23 19:42:50 +0000

    IDM-263: Removing unneeded import

--- Source/springidm/src/main/java/com/rackspace/idm/domain/dao/impl/LdapRepository.java
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/dao/impl/LdapRepository.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.configuration.Configuration;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tuckey.web.filters.urlrewrite.utils.StringUtils;
