72d711c7a266a8004fc241b117b4cc8d40c45431
devdatta.kulkarni <devdatta.kulkarni@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-03-31 17:23:54 +0000

    IDM-250 "Removing earlier attributes."

--- Source/springidm/src/main/java/com/rackspace/idm/util/MemcacheMBean.java
+++ Source/springidm/src/main/java/com/rackspace/idm/util/MemcacheMBean.java
@@ -6,28 +6,41 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import net.spy.memcached.MemcachedClient;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
 
-import com.unboundid.ldap.sdk.LDAPConnection;
-import com.unboundid.ldap.sdk.LDAPException;
-
 @ManagedResource
 public class MemcacheMBean {
 	 @Autowired
-    private MemcachedClient memcached;
+	 private MemcachedClient memcached;
+
+	 @ManagedAttribute
+	 public Map<String,String> getServerConnectionStatus() {
+	      
+	        Map<String,String> result = new HashMap<String, String>();
+	        
+	        List<String> availableServerList = getAvailableServers();
+	        List<String> unavailableServerList = getUnavailableServers();
+	        
+	        for(String server : availableServerList) {
+	            server = server.replaceAll("/", "");
+	            result.put(server, "up");
+	        }
+	        
+	        for(String server : unavailableServerList) {
+	            server = server.replaceAll("/", "");
+	            result.put(server, "down");
+	        }
+	        
+	        return result;
+	    }	
 	 
-	@ManagedAttribute
-	public List<String> getAvailableServers() {
+	private List<String> getAvailableServers() {
 		List<String> list = new ArrayList<String>();
-		Map<SocketAddress, Map<String, String>> stats = memcached.getStats();
 		Collection<SocketAddress> servers = memcached.getAvailableServers();
 		for (SocketAddress socketAddress : servers) {
 			list.add(socketAddress.toString());
@@ -35,10 +48,8 @@ public class MemcacheMBean {
 		return list;
 	}
 	
-	@ManagedAttribute
-	public List<String> getUnavailableServers() {
+	private List<String> getUnavailableServers() {
 		List<String> list = new ArrayList<String>();
-		Map<SocketAddress, Map<String, String>> stats = memcached.getStats();
 		Collection<SocketAddress> servers = memcached.getUnavailableServers();
 		for (SocketAddress socketAddress : servers) {
 			list.add(socketAddress.toString());
@@ -46,24 +57,5 @@ public class MemcacheMBean {
 		return list;
 	}
 	
-	@ManagedAttribute
-    public Map<String,String> getServerConnectionStatus() {
-      
-	    Map<String,String> result = new HashMap<String, String>();
-	    
-	    List<String> availableServerList = getAvailableServers();
-	    List<String> unavailableServerList = getUnavailableServers();
-	    
-	    for(String server : availableServerList) {
-	        server = server.replaceAll("/", "");
-	        result.put(server, "up");
-	    }
-	    
-	    for(String server : unavailableServerList) {
-	        server = server.replaceAll("/", "");
-	        result.put(server, "down");
-	    }
-	    
-	    return result;
-	}
+	
 }
