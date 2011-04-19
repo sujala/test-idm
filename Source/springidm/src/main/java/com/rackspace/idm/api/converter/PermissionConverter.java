a379530799c604f63784a2c4591244d651ca293f
john.eo <john.eo@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-04-19 21:10:25 +0000

    IDM-287: Modified the JAXB schema and the converter to handle the new attributes.

--- Source/springidm/src/main/java/com/rackspace/idm/api/converter/PermissionConverter.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/converter/PermissionConverter.java
@@ -14,47 +14,50 @@ public class PermissionConverter {
     public PermissionConverter() {
     }
 
-    public Permission toPermissionDO(
-        com.rackspace.idm.jaxb.Permission permission) {
-        Permission perm = new Permission();
-        perm.setClientId(permission.getClientId());
-        perm.setCustomerId(permission.getCustomerId());
-        perm.setPermissionId(permission.getPermissionId());
-        perm.setType(permission.getType());
-        perm.setValue(permission.getValue());
-        return perm;
+    public Permission toPermissionDO(com.rackspace.idm.jaxb.Permission permJaxb) {
+        Permission permDo = new Permission();
+        permDo.setClientId(permJaxb.getClientId());
+        permDo.setCustomerId(permJaxb.getCustomerId());
+        permDo.setPermissionId(permJaxb.getPermissionId());
+        permDo.setType(permJaxb.getType());
+        permDo.setValue(permJaxb.getValue());
+        permDo.setTitle(permJaxb.getTitle());
+        permDo.setDescription(permJaxb.getDescription());
+        permDo.setGrantedByDefault(permJaxb.isGrantedByDefault());
+        permDo.setEnabled(permJaxb.isEnabled());
+        return permDo;
     }
 
-    public List<Permission> toPermissionListDO(
-        com.rackspace.idm.jaxb.PermissionList permissions) {
+    public List<Permission> toPermissionListDO(com.rackspace.idm.jaxb.PermissionList permissions) {
         List<Permission> perms = new ArrayList<Permission>();
 
-        for (com.rackspace.idm.jaxb.Permission perm : permissions
-            .getPermissions()) {
+        for (com.rackspace.idm.jaxb.Permission perm : permissions.getPermissions()) {
             perms.add(toPermissionDO(perm));
         }
 
         return perms;
     }
 
-    public com.rackspace.idm.jaxb.Permission toPermissionJaxb(
-        Permission permission) {
-        com.rackspace.idm.jaxb.Permission perm = of.createPermission();
-        perm.setClientId(permission.getClientId());
-        perm.setCustomerId(permission.getCustomerId());
-        perm.setPermissionId(permission.getPermissionId());
-        perm.setType(permission.getType());
-        perm.setValue(permission.getValue());
-        return perm;
+    public com.rackspace.idm.jaxb.Permission toPermissionJaxb(Permission permDo) {
+        com.rackspace.idm.jaxb.Permission permJaxb = of.createPermission();
+        permJaxb.setClientId(permDo.getClientId());
+        permJaxb.setCustomerId(permDo.getCustomerId());
+        permJaxb.setPermissionId(permDo.getPermissionId());
+        permJaxb.setType(permDo.getType());
+        permJaxb.setValue(permDo.getValue());
+        permJaxb.setTitle(permDo.getTitle());
+        permJaxb.setDescription(permDo.getDescription());
+        permJaxb.setGrantedByDefault(permDo.isGrantedByDefault());
+        permJaxb.setEnabled(permDo.isEnabled());
+        return permJaxb;
     }
 
-    public com.rackspace.idm.jaxb.PermissionList toPermissionListJaxb(
-        List<Permission> permissions) {
-        
+    public com.rackspace.idm.jaxb.PermissionList toPermissionListJaxb(List<Permission> permissions) {
+
         if (permissions == null || permissions.size() < 1) {
             return null;
         }
-        
+
         com.rackspace.idm.jaxb.PermissionList perms = of.createPermissionList();
 
         for (Permission perm : permissions) {
@@ -63,8 +66,7 @@ public class PermissionConverter {
         return perms;
     }
 
-    public com.rackspace.idm.jaxb.Permissions toPermissionsJaxb(
-        PermissionSet permset) {
+    public com.rackspace.idm.jaxb.Permissions toPermissionsJaxb(PermissionSet permset) {
         com.rackspace.idm.jaxb.Permissions perms = of.createPermissions();
 
         if (permset.getDefineds() != null && permset.getDefineds().size() > 0) {
