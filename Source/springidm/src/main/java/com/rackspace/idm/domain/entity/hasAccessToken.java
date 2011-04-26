75221dd91093e6f8058b1768b6abce0b82e4ea45
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-04-26 21:25:24 +0000

    IDM-325: Updating hasAccessToken hasRefreshToken interfaces

--- Source/springidm/src/main/java/com/rackspace/idm/domain/entity/hasAccessToken.java
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/entity/hasAccessToken.java
@@ -1,5 +1,7 @@
 package com.rackspace.idm.domain.entity;
 
+import java.util.Date;
+
 import org.joda.time.DateTime;
 
 public interface hasAccessToken {
@@ -7,4 +9,10 @@ public interface hasAccessToken {
     void setAccessTokenExpired();
     
     boolean isAccessTokenExpired(DateTime time);
+    
+    String getAccessTokenString();
+    
+    void setAccessTokenString(String accessTokenString);
+    
+    void setAccessTokenExp(Date accessTokenExp);
 }
--- Source/springidm/src/main/java/com/rackspace/idm/domain/entity/hasRefreshToken.java
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/entity/hasRefreshToken.java
@@ -1,5 +1,7 @@
 package com.rackspace.idm.domain.entity;
 
+import java.util.Date;
+
 import org.joda.time.DateTime;
 
 public interface hasRefreshToken {
@@ -7,4 +9,10 @@ public interface hasRefreshToken {
     void setRefreshTokenExpired();
     
     boolean isRefreshTokenExpired(DateTime time);
+    
+    String getRefreshTokenString();
+    
+    void setRefreshTokenString(String refreshTokenString);
+    
+    void setRefreshTokenExp(Date refreshTokenExp);
 }
