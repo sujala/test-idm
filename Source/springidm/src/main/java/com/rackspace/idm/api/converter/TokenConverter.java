2627b06d87086ac9de6fd3a07508a9899bc22869
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-04-27 22:01:25 +0000

    IDM-326: removing unneeded imports

--- Source/springidm/src/main/java/com/rackspace/idm/api/converter/TokenConverter.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/converter/TokenConverter.java
@@ -6,11 +6,10 @@ import org.joda.time.DateTime;
 import org.joda.time.Seconds;
 
 import com.rackspace.idm.domain.entity.AccessToken;
-import com.rackspace.idm.domain.entity.RefreshToken;
 import com.rackspace.idm.jaxb.ObjectFactory;
 
 public class TokenConverter {
-    private ObjectFactory of = new ObjectFactory();
+    private final ObjectFactory of = new ObjectFactory();
 
     public com.rackspace.idm.jaxb.Token toTokenJaxb(String tokenString, Date expiration) {
         com.rackspace.idm.jaxb.Token jaxbToken = of.createToken();
