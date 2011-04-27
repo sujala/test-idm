cfe504bfb017f2855159da5604db9cd081756fb8
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-04-27 01:50:13 +0000

    IDM-326: removing unneeded imports

--- Source/springidm/src/main/java/com/rackspace/idm/util/MemcacheMBean.java
+++ Source/springidm/src/main/java/com/rackspace/idm/util/MemcacheMBean.java
@@ -1,10 +1,8 @@
 package com.rackspace.idm.util;
 
 import java.net.SocketAddress;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import net.spy.memcached.MemcachedClient;
