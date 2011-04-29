3b369590bc866b25bdd239b9f09207a82e9e11be
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-04-29 19:19:55 +0000

    IDM-326: updating permission object to have type

--- Source/springidm/src/main/java/com/rackspace/idm/domain/entity/PermissionObject.java
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/entity/PermissionObject.java
@@ -45,6 +45,9 @@ public class PermissionObject implements Auditable {
     @LDAPField(attribute = "resourceGroup", objectClass = "clientPermission", inRDN = false, filterUsage = FilterUsage.ALWAYS_ALLOWED, requiredForEncode = false)
     private String            resourceGroup;
 
+    @LDAPField(attribute = "permissionType", objectClass = "clientPermission", inRDN = false, filterUsage = FilterUsage.ALWAYS_ALLOWED, requiredForEncode = false)
+    private String            type;
+
     public PermissionObject() {
     }
 
@@ -71,6 +74,14 @@ public class PermissionObject implements Auditable {
 
     public void setLdapEntry(ReadOnlyEntry ldapEntry) {
         this.ldapEntry = ldapEntry;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getClientId() {
