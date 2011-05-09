e5d7f346d07983c0ad484d6ee62fee94d430c699
brad.brown <brad.brown@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-05-09 17:20:26 +0000

    IDM-343: Class Name re-factoring

---
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/config/JAXBContextResolver.java
@@ -0,0 +1,25 @@
+package com.rackspace.idm.domain.config;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBContext;
+
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.api.json.JSONJAXBContext;
+
+@Provider
+public class JAXBContextResolver implements ContextResolver<JAXBContext> {
+
+    private JAXBContext context;
+    //private Class[] types = {AuthCredentials.class};
+    
+    public JAXBContextResolver() throws Exception {
+        this.context = new JSONJAXBContext(
+                JSONConfiguration.natural().build(),
+                "com.rackspace.idm.jaxb");
+    }
+
+    public JAXBContext getContext(Class<?> objectType) {
+        return context;
+    }
+}
