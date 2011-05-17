f9a68f93dcc187a90ac17a8dcd0af7ee6df6edb2
matt.kovacs <matt.kovacs@1cc58c6c-e5e6-0310-8f1e-9ea522a2c4ea>
2011-05-17 20:49:45 +0000

    IDM-389: DelegatedPermission

---
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/entity/DelegatedPermission.java
@@ -0,0 +1,87 @@
+package com.rackspace.idm.domain.entity;
+
+import java.util.Arrays;
+
+import com.rackspace.idm.domain.dao.impl.LdapRepository;
+import com.unboundid.ldap.sdk.ReadOnlyEntry;
+import com.unboundid.ldap.sdk.persist.FilterUsage;
+import com.unboundid.ldap.sdk.persist.LDAPEntryField;
+import com.unboundid.ldap.sdk.persist.LDAPField;
+import com.unboundid.ldap.sdk.persist.LDAPGetter;
+import com.unboundid.ldap.sdk.persist.LDAPObject;
+
+@LDAPObject(structuralClass = LdapRepository.OBJECTCLASS_DELEGATEDPERMISSION,requestAllAttributes=true)
+public class DelegatedPermission extends Permission implements Auditable {
+    
+    @LDAPEntryField()
+    private ReadOnlyEntry ldapEntry;
+    @LDAPField(attribute = LdapRepository.ATTR_RESOURCE_GROUP, objectClass = LdapRepository.OBJECTCLASS_DELEGATEDPERMISSION, inRDN = false, filterUsage = FilterUsage.ALWAYS_ALLOWED, requiredForEncode = false)
+    private String[] resourceGroups;
+    
+    public DelegatedPermission() {}
+    
+    @Override
+    @LDAPGetter(attribute=LdapRepository.ATTR_NAME, inRDN=true)
+    public String getPermissionId() {
+        return super.getPermissionId();
+    }
+    
+    @Override
+    public String getUniqueId() {
+        if (ldapEntry == null) {
+            return null;
+        }
+        else {
+            return ldapEntry.getDN();
+        }
+    }
+
+    public String[] getResourceGroups() {
+        return resourceGroups;
+    }
+
+    public void setResourceGroups(String[] resourceGroups) {
+        this.resourceGroups = resourceGroups;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result
+            + ((ldapEntry == null) ? 0 : ldapEntry.hashCode());
+        result = prime * result + Arrays.hashCode(resourceGroups);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        DelegatedPermission other = (DelegatedPermission) obj;
+        if (ldapEntry == null) {
+            if (other.ldapEntry != null) {
+                return false;
+            }
+        } else if (!ldapEntry.equals(other.ldapEntry)) {
+            return false;
+        }
+        if (!Arrays.equals(resourceGroups, other.resourceGroups)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void copyChanges(DelegatedPermission modified) {
+        if (modified.getResourceGroups() != null) {
+            setResourceGroups(modified.getResourceGroups());
+        }
+    }
+}
