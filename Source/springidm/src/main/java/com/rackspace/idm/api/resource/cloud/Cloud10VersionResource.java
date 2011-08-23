7f85a1feb611d385c3319d2f3ff91305c8871760
matt.kovacs <matt.kovacs@rackspace.com>
2011-08-23 09:58:39 -0500

    adding header params to cloud v10 resource

--- Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/Cloud10VersionResource.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/Cloud10VersionResource.java
@@ -1,17 +1,19 @@
 package com.rackspace.idm.api.resource.cloud;
 
-import org.apache.commons.configuration.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
+
+import org.apache.commons.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Cloud Auth 1.0 API Version
@@ -22,9 +24,30 @@ import java.io.IOException;
 @Component
 public class Cloud10VersionResource {
 
+    public static final String HEADER_AUTH_TOKEN = "X-Auth-Token";
+    @Deprecated
+    public static final String HEADER_STORAGE_TOKEN = "X-Storage-Token";
+    @Deprecated
+    public static final String HEADER_STORAGE_URL = "X-Storage-Url";
+    public static final String HEADER_CDN_URL = "X-CDN-Management-Url";
+    public static final String HEADER_SERVER_MANAGEMENT_URL = "X-Server-Management-Url";
+    public static final String HEADER_STORAGE_INTERNAL_URL = "X-Storage-Internal-Url";
+
+    public static final String HEADER_AUTH_USER = "X-Auth-User";
+    public static final String HEADER_AUTH_KEY = "X-Auth-Key";
+    public static final String HEADER_STORAGE_USER = "X-Storage-User";
+    public static final String HEADER_STORAGE_PASS = "X-Storage-Pass";
+
+    public static final String AUTH_V1_0_FAILED_MSG = "Bad username or password";
+    public static final String AUTH_V1_0_USR_DISABLED_MSG = "Account disabled";
+
+    public static final String SERVICENAME_CLOUD_FILES = "cloudFiles";
+    public static final String SERVICENAME_CLOUD_FILES_CDN = "cloudFilesCDN";
+    public static final String SERVICENAME_CLOUD_SERVERS = "cloudServers";
+
     private final Configuration config;
     private final CloudClient cloudClient;
-    
+
     @Autowired
     public Cloud10VersionResource(Configuration config, CloudClient cloudClient) {
         this.config = config;
@@ -32,21 +55,12 @@ public class Cloud10VersionResource {
     }
 
     @GET
-    public Response getCloud10VersionInfo(
-    	@Context HttpHeaders httpHeaders		
-    ) throws IOException {
+    public Response getCloud10VersionInfo(@Context HttpHeaders httpHeaders,
+        @HeaderParam(HEADER_AUTH_USER) String username,
+        @HeaderParam(HEADER_AUTH_KEY) String key) throws IOException {
         return cloudClient.get(getCloudAuthV10Url(), httpHeaders).build();
     }
 
-//    @GET
-//    public Response authenticate(
-//            @HeaderParam("X-Auth-User") String  username,
-//            @HeaderParam("X-Auth-Key") String  key){
-//
-//        //TODO forward call to Auth 1.0
-//        return Response.ok().build();
-//    }
-    
     private String getCloudAuthV10Url() {
         return config.getString("cloudAuth10url");
     }
