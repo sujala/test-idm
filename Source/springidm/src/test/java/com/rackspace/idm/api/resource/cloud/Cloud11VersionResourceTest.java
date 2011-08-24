badc60059575523be0dbea25ecc2f77491203e01
hector <hector.munoz@rackspace.com>
2011-08-24 10:28:42 -0500

    added test for auth 1.1

--- Source/springidm/src/test/java/com/rackspace/idm/api/resource/cloud/Cloud11VersionResourceTest.java
+++ Source/springidm/src/test/java/com/rackspace/idm/api/resource/cloud/Cloud11VersionResourceTest.java
@@ -5,6 +5,8 @@ import com.sun.jersey.api.client.WebResource;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.core.MediaType;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -26,6 +28,24 @@ public class Cloud11VersionResourceTest extends AbstractAroundClassJerseyTest{
         WebResource resource = resource().path("cloud/v1.1");
         ClientResponse clientResponse = resource.get(ClientResponse.class);
         assertThat("response code", clientResponse.getStatus(), equalTo(200));
+    }
+
+    @Test
+    public void authenticate_withValidUser_returns200(){
+        WebResource resource = resource().path("cloud/v1.1/auth");
+        ClientResponse clientResponse = resource
+                .type(MediaType.APPLICATION_XML_TYPE)
+                .post(ClientResponse.class, "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"cmarin1\" key=\"70b67400a497d8148987d083b35caf9d\" />");
+        assertThat("response code", clientResponse.getStatus(), equalTo(200));
+    }
+
+    @Test
+    public void authenticate_withInvalidUser_returns401(){
+        WebResource resource = resource().path("cloud/v1.1/auth");
+        ClientResponse clientResponse = resource
+                .type(MediaType.APPLICATION_XML_TYPE)
+                .post(ClientResponse.class, "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"badUser\" key=\"70b67400a497d8148987d083b35caf9d\" />");
+        assertThat("response code", clientResponse.getStatus(), equalTo(401));
     }
 
 }
