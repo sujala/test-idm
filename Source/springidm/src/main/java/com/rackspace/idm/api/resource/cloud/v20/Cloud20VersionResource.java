65181382b7d641eadd8d6d604059366089df3391
werner mendizabal <nonameentername@gmail.com>
2011-09-20 09:46:16 -0500

    update cloud20versionresource to handle request

--- Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/v20/Cloud20VersionResource.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/v20/Cloud20VersionResource.java
@@ -71,43 +71,43 @@ public class Cloud20VersionResource {
     @Path("tokens/{tokenId}")
     public Response validateToken(@Context HttpHeaders httpHeaders, @PathParam("tokenId") String tokenId,
         @QueryParam("belongsTo") String belongsTo) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().validateToken(httpHeaders, tokenId, belongsTo).build();
     }
 
     @GET
     @Path("tokens/{tokenId}/endpoints")
     public Response listEndpointsForToken(@Context HttpHeaders httpHeaders, @PathParam("tokenId") String tokenId) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().listEndpointsForToken(httpHeaders, tokenId).build();
     }
 
     @GET
     @Path("extensions")
     public Response listExtensions(@Context HttpHeaders httpHeaders) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().listExtensions(httpHeaders).build();
     }
 
     @GET
     @Path("extensions/{alias}")
     public Response getExtension(@Context HttpHeaders httpHeaders, @PathParam("alias") String alias) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().getExtension(httpHeaders, alias).build();
     }
 
     @GET
     @Path("users")
     public Response getUserByName(@Context HttpHeaders httpHeaders, @QueryParam("name") String name) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().getUserByName(httpHeaders, name).build(); 
     }
 
     @GET
     @Path("users/{userId}")
     public Response getUserById(@Context HttpHeaders httpHeaders, @PathParam("userId") String userId) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().getUserById(httpHeaders, userId).build();
     }
 
     @GET
     @Path("users/{userId}/roles")
     public Response listUserGlobalRoles(@Context HttpHeaders httpHeaders, @PathParam("userId") String userId) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().listUserGlobalRoles(httpHeaders, userId).build();
     }
 
     @GET
@@ -116,51 +116,55 @@ public class Cloud20VersionResource {
         @Context HttpHeaders httpHeaders, @QueryParam("name") String name, @QueryParam("marker") String marker,
         @QueryParam("limit") Integer limit) throws IOException {
         //Note: getTenantByName only available to admin
-        return Response.ok().build(); 
+        if(name != null) {
+            return getCloud20Service().getTenantByName(httpHeaders, name).build();
+        } else {
+            return getCloud20Service().listTenants(httpHeaders, marker, limit).build();
+        }
     }
 
     @GET
     @Path("tenants/{tenantId}")
     public Response getTenantById(@Context HttpHeaders httpHeaders, @PathParam("tenantId") String tenantsId) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().getTenantById(httpHeaders, tenantsId).build();
     }
 
     @POST
     @Path("users/credentials")
     public Response addUserCredential(@Context HttpHeaders httpHeaders, String body) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().addUserCredential(httpHeaders, body).build();
     }
 
     @GET
     @Path("users/credentials")
     public Response listCredentials(@Context HttpHeaders httpHeaders, @QueryParam("marker") String marker,
         @QueryParam("limit") Integer limit) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().listCredentials(httpHeaders, marker, limit).build();
     }
 
     @POST
     @Path("users/credentials/RAX-KSKEY:apikeyCredentials")
     public Response updateUserCredential(@Context HttpHeaders httpHeaders, String body) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().updateUserCredential(httpHeaders, body).build();
     }
 
     @GET
     @Path("users/credentials/RAX-KSKEY:apikeyCredentials")
     public Response getUserCredential(@Context HttpHeaders httpHeaders) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().getUserCredential(httpHeaders).build();
     }
 
     @DELETE
     @Path("users/credentials/RAX-KSKEY:apikeyCredentials")
     public Response deleteUserCredential(@Context HttpHeaders httpHeaders) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().deleteUserCredential(httpHeaders).build();
     }
 
     @GET
-    @Path("tenants/{tenantsId}/users/{userId}")
-    public Response listRolesForUserOnTenant(@Context HttpHeaders httpHeaders, @PathParam("tenantsId") String tenantsId,
+    @Path("tenants/{tenantId}/users/{userId}")
+    public Response listRolesForUserOnTenant(@Context HttpHeaders httpHeaders, @PathParam("tenantId") String tenantId,
         @PathParam("userId") String userId) throws IOException {
-        return Response.ok().build(); 
+        return getCloud20Service().listRolesForUserOnTenant(httpHeaders, tenantId, userId).build();
     }
 
     private Cloud20Service getCloud20Service() {
