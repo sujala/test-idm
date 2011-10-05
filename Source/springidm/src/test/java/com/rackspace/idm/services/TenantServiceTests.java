bfab5c3e2a4d34cb0a96f04ffaadd3821960a143
hector <hector.munoz@rackspace.com>
2011-10-05 16:28:16 -0500

    fixed test for DefaultTenantService

--- Source/springidm/src/test/java/com/rackspace/idm/services/TenantServiceTests.java
+++ Source/springidm/src/test/java/com/rackspace/idm/services/TenantServiceTests.java
@@ -1,13 +1,5 @@
 package com.rackspace.idm.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.easymock.EasyMock;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 import com.rackspace.idm.domain.dao.ClientDao;
 import com.rackspace.idm.domain.dao.ScopeAccessDao;
 import com.rackspace.idm.domain.dao.TenantDao;
@@ -18,6 +10,13 @@ import com.rackspace.idm.domain.entity.TenantRole;
 import com.rackspace.idm.domain.service.TenantService;
 import com.rackspace.idm.domain.service.impl.DefaultTenantService;
 import com.rackspace.idm.exception.NotFoundException;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TenantServiceTests {
 
@@ -52,6 +51,7 @@ public class TenantServiceTests {
     @Test
     public void shouldAddTenant() {
         Tenant tenant = getTestTenant();
+        EasyMock.expect(mockTenantDao.getTenantByName(tenantName)).andReturn(null);
         EasyMock.expect(mockTenantDao.getNextTenantId()).andReturn(tenantId1);
         mockTenantDao.addTenant(tenant);
         EasyMock.replay(mockTenantDao);
