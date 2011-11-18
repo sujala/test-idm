dbb85eff2fee92a68cd1fc4c0cc2bf10b8d0a3fd
hector <hector.munoz@rackspace.com>
2011-11-18 09:02:32 -0600

    updated copyProperties.sh to include log4j.xml

--- Source/springidm/copyProperties.sh
+++ Source/springidm/copyProperties.sh
@@ -6,5 +6,5 @@ then
 else
     HOST=$1
     ENV=$2
-    scp {./src/main/config/$ENV/idm.properties,./src/main/config/base.idm.properties,./src/main/config/$ENV/idm.secrets.properties} rack@$HOST:/etc/idm/
+    scp {./src/main/config/$ENV/idm.properties,./src/main/config/base.idm.properties,./src/main/config/$ENV/idm.secrets.properties,./src/main/config/log4j.xml} rack@$HOST:/etc/idm/
 fi
