b85dff633f7188ebddc02d45500ff79964277a1a
hector <hector.munoz@rackspace.com>
2011-11-29 19:09:50 -0600

    fixed bug where global auth impl was returning 500

--- Source/springidm/src/main/java/com/rackspace/idm/api/converter/cloudv11/AuthConverterCloudV11.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/converter/cloudv11/AuthConverterCloudV11.java
@@ -1,27 +1,23 @@
 package com.rackspace.idm.api.converter.cloudv11;
 
-import java.util.List;
-
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-
-import org.apache.commons.configuration.Configuration;
-import org.joda.time.DateTime;
-
 import com.rackspace.idm.domain.entity.CloudEndpoint;
 import com.rackspace.idm.domain.entity.UserScopeAccess;
 import com.rackspacecloud.docs.auth.api.v1.AuthData;
 import com.rackspacecloud.docs.auth.api.v1.FullToken;
 import com.rackspacecloud.docs.auth.api.v1.ServiceCatalog;
+import org.apache.commons.configuration.Configuration;
+import org.joda.time.DateTime;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import java.util.List;
 
 public class AuthConverterCloudV11 {
     private final Configuration config;
     private final TokenConverterCloudV11 tokenConverter;
     private final EndpointConverterCloudV11 endpointConverter;
 
-    public AuthConverterCloudV11(Configuration config,
-        TokenConverterCloudV11 tokenConverter,
-        EndpointConverterCloudV11 endpointConverter) {
+    public AuthConverterCloudV11(Configuration config, TokenConverterCloudV11 tokenConverter, EndpointConverterCloudV11 endpointConverter) {
         this.config = config;
         this.tokenConverter = tokenConverter;
         this.endpointConverter = endpointConverter;
@@ -29,16 +25,14 @@ public class AuthConverterCloudV11 {
 
     private final com.rackspacecloud.docs.auth.api.v1.ObjectFactory OBJ_FACTORY = new com.rackspacecloud.docs.auth.api.v1.ObjectFactory();
 
-    public AuthData toCloudv11AuthDataJaxb(UserScopeAccess usa,
-        List<CloudEndpoint> endpoints) {
+    public AuthData toCloudv11AuthDataJaxb(UserScopeAccess usa, List<CloudEndpoint> endpoints) {
 
         AuthData auth = OBJ_FACTORY.createAuthData();
 
         auth.setToken(this.tokenConverter.toCloudv11TokenJaxb(usa));
 
         if (endpoints != null && endpoints.size() > 0) {
-            ServiceCatalog catalog = this.endpointConverter
-                .toServiceCatalog(endpoints);
+            ServiceCatalog catalog = this.endpointConverter.toServiceCatalog(endpoints);
             auth.setServiceCatalog(catalog);
         }
 
@@ -55,14 +49,10 @@ public class AuthConverterCloudV11 {
         try {
             if (usa.getAccessTokenExp() != null) {
                 token.setCreated(DatatypeFactory.newInstance()
-                    .newXMLGregorianCalendar(
-                        new DateTime(usa.getAccessTokenExp()).minusSeconds(
-                            getCloudAuthExpirationSeconds())
-                            .toGregorianCalendar()));
+                    .newXMLGregorianCalendar(new DateTime(usa.getAccessTokenExp()).minusSeconds(
+                            getCloudAuthExpirationSeconds()).toGregorianCalendar()));
                 token.setExpires(DatatypeFactory.newInstance()
-                    .newXMLGregorianCalendar(
-                        new DateTime(usa.getAccessTokenExp())
-                            .toGregorianCalendar()));
+                    .newXMLGregorianCalendar(new DateTime(usa.getAccessTokenExp()).toGregorianCalendar()));
             }
 
         } catch (DatatypeConfigurationException e) {
@@ -78,6 +68,6 @@ public class AuthConverterCloudV11 {
     }
     
     private String getCloudUserRefString() {
-        return config.getString("cloud.user.ref.string");
+        return config.getString("cloud.user.ref.string")+"v1.1/users/%s";
     }
 }
