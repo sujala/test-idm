1f8e2766a18b9ab0c503349f7a9d494003f14917
Jorge Munoz <jorge.munoz@rackspace.com>
2012-05-02 15:23:50 -0500

    Removed the ssh path to copyProperties

--- Source/springidm/copyProperties.sh
+++ Source/springidm/copyProperties.sh
@@ -2,15 +2,15 @@
 
 BASEDIR=$(dirname $(readlink -f $0))
 
-if [ $# -ne 3 ]
+if [ $# -ne 2 ]
 then
-    echo "usage: copyPropertiesgcsh <host> <environment> <.ssh path>"
+    echo "usage: copyPropertiesgcsh <host> <environment>"
 else
     HOST=$1
     ENV=$2
     SSHPATH=$3
-    scp -i $SSHPATH/id_rsa $BASEDIR/src/main/config/$ENV/idm.properties gauth@$HOST:/home/gauth/config/
-    scp -i $SSHPATH/id_rsa $BASEDIR/src/main/config/base.idm.properties gauth@$HOST:/home/gauth/config/
-    scp -i $SSHPATH/id_rsa  $BASEDIR/src/main/config/$ENV/idm.secrets.properties gauth@$HOST:/home/gauth/config/
-    scp -i $SSHPATH/id_rsa  $BASEDIR/src/main/config/log4j.xml gauth@$HOST:/home/gauth/config/
+    scp $BASEDIR/src/main/config/$ENV/idm.properties gauth@$HOST:/home/gauth/config/
+    scp $BASEDIR/src/main/config/base.idm.properties gauth@$HOST:/home/gauth/config/
+    scp $BASEDIR/src/main/config/$ENV/idm.secrets.properties gauth@$HOST:/home/gauth/config/
+    scp $BASEDIR/src/main/config/log4j.xml gauth@$HOST:/home/gauth/config/
 fi
