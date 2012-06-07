12d9cde377bd75f7d0e57e23617e87daa8b3e9e0
Jorge Munoz <jorge.munoz@rackspace.com>
2012-06-07 18:23:17 -0500

    Removed async from atomhopper

--- Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/atomHopper/AtomHopperClient.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/atomHopper/AtomHopperClient.java
@@ -39,7 +39,7 @@ public class AtomHopperClient {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Async
+    //@Async
     public void postUser(User user, String authToken, String userStatus) throws JAXBException, IOException, HttpException, URISyntaxException {
         try {
             AtomFeed atomFeed = createAtomFeed(user);
