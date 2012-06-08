ae30456e47f4d5ebb622cb88ab756fcad6b24f83
Jorge Munoz <jorge.munoz@rackspace.com>
2012-06-08 14:07:27 -0500

    Modified some test

--- Source/springidm/src/test/java/com/rackspace/idm/api/resource/cloud/v11/Cloud11VersionResourceTest.java
+++ Source/springidm/src/test/java/com/rackspace/idm/api/resource/cloud/v11/Cloud11VersionResourceTest.java
@@ -83,6 +83,7 @@ public class Cloud11VersionResourceTest extends AbstractAroundClassJerseyTest {
     public void authenticate_withInvalidUser_returns401() {
         WebResource resource = resource().path("cloud/v1.1/auth");
         ClientResponse clientResponse = resource
+                .header("Authorization", "Basic badauth")
                 .type(MediaType.APPLICATION_XML_TYPE)
                 .post(ClientResponse.class, "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"badUser\" key=\"70b67400a497d8148987d083b35caf9d\" />");
         assertThat("response code", clientResponse.getStatus(), equalTo(401));
@@ -107,7 +108,7 @@ public class Cloud11VersionResourceTest extends AbstractAroundClassJerseyTest {
     @Test
     public void getUser_withoutAuth_returns401() {
         WebResource resource = resource().path("cloud/v1.1/users/a1b2c3");
-        ClientResponse clientResponse = resource.get(ClientResponse.class);
+        ClientResponse clientResponse = resource.header("Authorization", "Basic badauth").get(ClientResponse.class);
         assertThat("response code", clientResponse.getStatus(), equalTo(401));
     }
 }
