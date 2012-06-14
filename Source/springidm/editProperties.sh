2e338c006785b697e481b659d0ad2b45510fdd94
Jorge Munoz <jorge.munoz@rackspace.com>
2012-06-14 10:50:38 -0500

    Edit the editProperties.sh

--- Source/springidm/editProperties.sh
+++ Source/springidm/editProperties.sh
@@ -6,6 +6,6 @@ if [ $# -ne 2 ]
 then
     echo "usage: editProperties.sh <current> <update>"
 else
-    sed -e "s/$1/$2/g" src/main/config/base.idm.properties > src/main/config/temp
-    mv src/main/config/temp src/main/config/base.idm.properties
+    sed -e "s/$1/$2/g" $BASEDIR/src/main/config/base.idm.properties > $BASEDIR/src/main/config/temp
+    mv $BASEDIR/src/main/config/temp src/main/config/base.idm.properties
 fi
