213515e7ced24249625b5d0c038360ed81ebeb63
Kurt Kartaltepe <kurt.kartaltepe@rackspace.com>
2012-06-26 10:03:52 -0500

    Tests for InputValidator

--- Source/springidm/src/test/java/com/rackspace/idm/validation/InputValidatorTest.java
+++ Source/springidm/src/test/java/com/rackspace/idm/validation/InputValidatorTest.java
@@ -1,9 +1,17 @@
 package com.rackspace.idm.validation;
 
+import com.rackspace.idm.api.error.ApiError;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.validation.groups.Default;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,4 +30,17 @@ public class InputValidatorTest {
         spy = spy(inputValidator);
     }
 
+    @Test
+    public void validate_callsValidate_with400Status() throws Exception {
+        Object paramObj = new Object();
+        spy.validate(paramObj);
+        verify(spy).validate(paramObj, 400);
+    }
+
+    @Test
+    public void validate_withErrorStatusSet_withNoConstraints_returnsNull() throws Exception {
+        Object paramObj = new Object();
+        ApiError error = inputValidator.validate(paramObj, 400);
+        assertThat("error", error, nullValue());
+    }
 }
