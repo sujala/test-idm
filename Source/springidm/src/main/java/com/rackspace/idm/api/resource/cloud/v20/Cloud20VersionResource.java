562029c5e063f3fd591e38a7df526b51f6bc13d9
Ryan Clements <ryan5034@ryan5034-Latitude-E6400.(none)>
2012-07-13 17:49:39 -0500

    Fixed soapUI tests

--- Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/v20/Cloud20VersionResource.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/v20/Cloud20VersionResource.java
@@ -58,7 +58,7 @@ public class Cloud20VersionResource {
     @GET
     public Response getCloud20VersionInfo() throws JAXBException {
         String responseXml = cloudContractDescriptionBuilder.buildVersion20Page();
-        JAXBContext context = JAXBContext.newInstance(VersionChoice.class);
+        JAXBContext context = JAXBContext.newInstance("org.openstack.docs.common.api.v1:org.w3._2005.atom");
         Unmarshaller unmarshaller = context.createUnmarshaller();
         JAXBElement<VersionChoice> versionChoice = (JAXBElement<VersionChoice>) unmarshaller.unmarshal(new StringReader(responseXml));
         return Response.ok(versionChoice.getValue()).build();
