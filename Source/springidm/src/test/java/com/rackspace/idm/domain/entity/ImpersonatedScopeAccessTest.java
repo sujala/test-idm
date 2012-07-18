166e2549d41959bc39bebf94879d52aca99eb0a5
Yung Lai <yung.lai@rackspace.com>
2012-07-18 18:39:30 -0500

    Testing cobertura coverage

---
+++ Source/springidm/src/test/java/com/rackspace/idm/domain/entity/ImpersonatedScopeAccessTest.java
@@ -0,0 +1,30 @@
+package com.rackspace.idm.domain.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yung Lai
+ * Date: 7/18/12
+ * Time: 6:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImpersonatedScopeAccessTest {
+    ImpersonatedScopeAccess impersonatedScopeAccess;
+
+    @Before
+    public void setUp() throws Exception {
+        impersonatedScopeAccess = new ImpersonatedScopeAccess();
+    }
+
+    @Test
+    public void getClientId_returnsClientId() throws Exception {
+        impersonatedScopeAccess.setClientId("clientId");
+        String result = impersonatedScopeAccess.getClientId();
+        assertThat("client id", result, equalTo("clientId"));
+    }
+}
