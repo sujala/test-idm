933f31b716f0dc0cc5195c189e07513605d19b1c
Yung Lai <yung.lai@rackspace.com>
2012-07-18 19:35:09 -0500

    Build should now work

--- Source/springidm/src/test/java/com/rackspace/idm/domain/entity/ImpersonatedScopeAccessTest.java
+++ Source/springidm/src/test/java/com/rackspace/idm/domain/entity/ImpersonatedScopeAccessTest.java
@@ -1,10 +1,15 @@
 package com.rackspace.idm.domain.entity;
 
+import com.unboundid.ldap.sdk.Attribute;
+import com.unboundid.ldap.sdk.ReadOnlyEntry;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,5 +38,12 @@ public class ImpersonatedScopeAccessTest {
         impersonatedScopeAccess.setLdapEntry(null);
         String result = impersonatedScopeAccess.getUniqueId();
         assertThat("unique id", result, equalTo(null));
+    }
+
+    @Test
+    public void getUniqueId_ldapEntryNotNull_returnsLdapEntryGetDn() throws Exception {
+        impersonatedScopeAccess.setLdapEntry(new ReadOnlyEntry("uniqueId",new Attribute[0]));
+        String result = impersonatedScopeAccess.getUniqueId();
+        assertThat("unique id", result, equalTo("uniqueId"));
     }
 }
