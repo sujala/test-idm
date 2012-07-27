2230030e4572fe0aa0ed9457c2e1ae929270be4d
Kurt Kartaltepe <kurt.kartaltepe@rackspace.com>
2012-07-27 10:08:54 -0500

    tests for AuthenticationResult and changed tests for ClientAuthenticationResult

--- Source/springidm/src/main/java/com/rackspace/idm/domain/entity/ClientAuthenticationResult.java
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/entity/ClientAuthenticationResult.java
@@ -27,13 +27,7 @@ public class ClientAuthenticationResult extends AuthenticationResult {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
         if (!super.equals(obj)) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
             return false;
         }
         ClientAuthenticationResult other = (ClientAuthenticationResult) obj;
