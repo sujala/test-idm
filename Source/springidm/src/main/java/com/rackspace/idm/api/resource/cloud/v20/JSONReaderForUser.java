8bb20a8945462be145f322eec3da4f9247024bd2
Kurt Kartaltepe <kurt.kartaltepe@rackspace.com>
2012-07-27 13:41:42 -0500

    removed lots of TODO's cleaned imports.

--- Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/v20/JSONReaderForUser.java
+++ Source/springidm/src/main/java/com/rackspace/idm/api/resource/cloud/v20/JSONReaderForUser.java
@@ -83,7 +83,6 @@ public class JSONReaderForUser implements MessageBodyReader<User> {
                 }
             }
         } catch (ParseException e) {
-            // TODO Auto-generated catch block
             logger.info(e.toString());
             throw new BadRequestException("Bad JSON request");
         }
