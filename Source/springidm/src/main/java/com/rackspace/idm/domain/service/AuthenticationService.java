1f842e362f8885f8c32d15952ce7782b10e3a249
Kurt Kartaltepe <kurt.kartaltepe@rackspace.com>
2012-08-02 12:13:16 -0500

    fixed modifiers rules compliance

--- Source/springidm/src/main/java/com/rackspace/idm/domain/service/AuthenticationService.java
+++ Source/springidm/src/main/java/com/rackspace/idm/domain/service/AuthenticationService.java
@@ -5,7 +5,7 @@ import com.rackspace.idm.domain.entity.Credentials;
 
 public interface AuthenticationService {
     
-	final String AUTH_TOKEN_HEADER = "X-Auth-Token";
+	String AUTH_TOKEN_HEADER = "X-Auth-Token";
 	
     AuthData authenticate(Credentials credentials);
     
