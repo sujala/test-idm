918afe5d2928a9904bb4b9eeffce129c3a5bba1a
Jorge Munoz <jorge.munoz@rackspace.com>
2012-08-16 13:07:16 -0500

    Modified editproperties.sh

--- scripts/editProperties.sh
+++ scripts/editProperties.sh
@@ -4,8 +4,8 @@ BASEDIR=$(dirname $(readlink -f $0))
 
 if [ $# -ne 3 ]
 then
-    echo "usage: editProperties.sh <current> <update> <ENV>"
+    echo "usage: editProperties.sh <current> <update>"
 else
-    sed -e "s/$1/$2/g" $BASEDIR/../src/main/config/$3/idm.properties > $BASEDIR/../src/main/config/$3/temp
-    mv $BASEDIR/../src/main/config/$3/temp $BASEDIR/../src/main/config/$3/idm.properties
+    sed -e "s/$1/$2/g" $BASEDIR/../src/main/config/base.idm.properties > $BASEDIR/../src/main/config/temp
+    mv $BASEDIR/../src/main/config/temp $BASEDIR/../src/main/config/base.idm.properties
 fi
