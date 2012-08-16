9ffd54b7fe61916cfb6ba7ac72504558a7e30ea3
unknown <shan0433@L7MJK5R1.RACKSPACE.CORP>
2012-08-16 12:24:50 -0500

    updated scripts

--- src/test/groovy/step_definitions/foundation/BaseFoundationSteps.groovy
+++ src/test/groovy/step_definitions/foundation/BaseFoundationSteps.groovy
@@ -1,6 +1,7 @@
 import cucumber.runtime.PendingException
 import org.junit.Before
 import com.sun.jersey.api.client.Client
+import com.sun.jersey.api.client.ClientResponse
 
 this.metaClass.mixin(cucumber.runtime.groovy.Hooks)
 this.metaClass.mixin(cucumber.runtime.groovy.EN)
@@ -10,3 +11,11 @@ Before() {
 //    jetty_host = System.properties.get("jetty.host")
     jetty_host = "http://localhost:8083/idm/"
 }
+
+//Version steps
+Given(~'^a foundation endpoint$') { ->
+    // Express the Regexp above with the code you wish you had
+    foundation_path = "v1"
+    foundation_endpoint = client.resource(jetty_host+foundation_path)
+}
+
