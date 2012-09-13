58facf3c1ded06da7264a200e06e1965a1dab4cf
Jorge Munoz <jorge.munoz@rackspace.com>
2012-09-13 11:27:14 -0500

    More integration test for policies

--- src/test/java/com/rackspace/idm/domain/dao/impl/LdapPolicyRepositoryIntegrationTest.java
+++ src/test/java/com/rackspace/idm/domain/dao/impl/LdapPolicyRepositoryIntegrationTest.java
@@ -2,6 +2,7 @@ package com.rackspace.idm.domain.dao.impl;
 
 import com.rackspace.idm.domain.config.LdapConfiguration;
 import com.rackspace.idm.domain.config.PropertyFileConfiguration;
+import com.rackspace.idm.domain.entity.Policies;
 import com.rackspace.idm.domain.entity.Policy;
 import junit.framework.TestCase;
 import org.junit.*;
@@ -68,13 +69,13 @@ public class LdapPolicyRepositoryIntegrationTest extends InMemoryLdapIntegration
     public void shouldAddGetDeletePolicy() {
         this.repo.addPolicy(getTestPolicy());
         Policy policy = repo.getPolicy(policyId);
-        Assert.assertEquals("compair blob", policy.getBlob(), "someBlob");
-        Assert.assertEquals("compair blob",policy.getName(),"XXXX");
-        Assert.assertEquals("compair blob",policy.getPolicyType(),"someType");
-        Assert.assertEquals("compair blob",policy.getDescription(),"Description");
-        Assert.assertEquals("compair blob",policy.isEnabled(),true);
-        Assert.assertEquals("compair blob",policy.isGlobal(),false);
-        Assert.assertEquals("compair blob",policy.getPolicyId(),"XXXX");
+        Assert.assertEquals("compare blob", policy.getBlob(), "someBlob");
+        Assert.assertEquals("compare name",policy.getName(),"XXXX");
+        Assert.assertEquals("compare Type",policy.getPolicyType(),"someType");
+        Assert.assertEquals("compare Description",policy.getDescription(),"Description");
+        Assert.assertEquals("compare enabled",policy.isEnabled(),true);
+        Assert.assertEquals("compare global",policy.isGlobal(),false);
+        Assert.assertEquals("compare id",policy.getPolicyId(),"XXXX");
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -98,13 +99,13 @@ public class LdapPolicyRepositoryIntegrationTest extends InMemoryLdapIntegration
     public void getPolicyByName_validPolicy() throws Exception {
         this.repo.addPolicy(getTestPolicy());
         Policy policy = this.repo.getPolicyByName(name);
-        Assert.assertEquals("compair blob", policy.getBlob(), "someBlob");
-        Assert.assertEquals("compair blob",policy.getName(),"XXXX");
-        Assert.assertEquals("compair blob",policy.getPolicyType(),"someType");
-        Assert.assertEquals("compair blob",policy.getDescription(),"Description");
-        Assert.assertEquals("compair blob",policy.isEnabled(),true);
-        Assert.assertEquals("compair blob",policy.isGlobal(),false);
-        Assert.assertEquals("compair blob",policy.getPolicyId(),"XXXX");
+        Assert.assertEquals("compare blob", policy.getBlob(), "someBlob");
+        Assert.assertEquals("compare name",policy.getName(),"XXXX");
+        Assert.assertEquals("compare type",policy.getPolicyType(),"someType");
+        Assert.assertEquals("compare description",policy.getDescription(),"Description");
+        Assert.assertEquals("compare enable",policy.isEnabled(),true);
+        Assert.assertEquals("compare global",policy.isGlobal(),false);
+        Assert.assertEquals("compare id",policy.getPolicyId(),"XXXX");
     }
 
     @Test
@@ -121,13 +122,40 @@ public class LdapPolicyRepositoryIntegrationTest extends InMemoryLdapIntegration
         update.setDescription("newDescription");
         this.repo.updatePolicy(update,policyId);
         Policy policy = repo.getPolicy(policyId);
-        Assert.assertEquals("compair blob", policy.getBlob(), "newBlob");
-        Assert.assertEquals("compair blob",policy.getName(),"XXXX");
-        Assert.assertEquals("compair blob",policy.getPolicyType(),"someType");
-        Assert.assertEquals("compair blob",policy.getDescription(),"newDescription");
-        Assert.assertEquals("compair blob",policy.isEnabled(),true);
-        Assert.assertEquals("compair blob",policy.isGlobal(),false);
-        Assert.assertEquals("compair blob",policy.getPolicyId(),"XXXX");
+        Assert.assertEquals("compare blob", policy.getBlob(), "newBlob");
+        Assert.assertEquals("compare name",policy.getName(),"XXXX");
+        Assert.assertEquals("compare type",policy.getPolicyType(),"someType");
+        Assert.assertEquals("compare description",policy.getDescription(),"newDescription");
+        Assert.assertEquals("compare enable",policy.isEnabled(),true);
+        Assert.assertEquals("compare global",policy.isGlobal(),false);
+        Assert.assertEquals("compare id",policy.getPolicyId(),"XXXX");
+    }
+
+    @Test
+    public void listPolicies_returnsAllPolicies() {
+        this.repo.addPolicy(getTestPolicy());
+        Policy policy = new Policy();
+        policy.setDescription("otherDes");
+        policy.setEnabled(true);
+        policy.setGlobal(false);
+        policy.setName("otherName");
+        policy.setBlob("someblob");
+        policy.setPolicyType("otherType");
+        policy.setPolicyId("321");
+        this.repo.addPolicy(policy);
+        Policies policies = repo.getPolicies();
+        Boolean result1 = false;
+        Boolean result2 = false;
+        for(Policy singlePolicy : policies.getPolicy()){
+            if(singlePolicy.getName().equals(policy.getName())) {
+                result1 = true;
+            }
+            if(singlePolicy.getName().equals("XXXX")){
+                result2 = true;
+            }
+        }
+        Assert.assertEquals("compare policies", result1, true);
+        Assert.assertEquals("compare policies", result2, true);
     }
 
     private Policy getTestPolicy() {
