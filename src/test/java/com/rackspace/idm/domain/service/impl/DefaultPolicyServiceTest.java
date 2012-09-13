107523280e21b70cfd097efd58c6ac165367b659
Jorge Munoz <jorge.munoz@rackspace.com>
2012-09-13 15:34:09 -0500

    Completed test for DefaultPolicyService

--- src/test/java/com/rackspace/idm/domain/service/impl/DefaultPolicyServiceTest.java
+++ src/test/java/com/rackspace/idm/domain/service/impl/DefaultPolicyServiceTest.java
@@ -4,14 +4,15 @@ import com.rackspace.idm.domain.dao.PolicyDao;
 import com.rackspace.idm.domain.dao.impl.LdapPolicyRepository;
 import com.rackspace.idm.domain.entity.Policies;
 import com.rackspace.idm.domain.entity.Policy;
+import com.rackspace.idm.exception.BadRequestException;
+import com.rackspace.idm.exception.NotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -45,23 +46,106 @@ public class DefaultPolicyServiceTest{
 
     }
 
-    @Test
-    public void testAddPolicy() throws Exception {
+    @Test(expected = BadRequestException.class)
+    public void testAddPolicy_nullPolicy_returnBadRequestException() throws Exception {
+        defaultPolicyService.addPolicy(null);
+    }
 
+    @Test(expected = BadRequestException.class)
+    public void testAddPolicy_nullName_returnBadRequestException() throws Exception {
+        Policy policy = new Policy();
+        policy.setPolicyId("123");
+        defaultPolicyService.addPolicy(policy);
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testAddPolicy_nullBlob_returnBadRequestException() throws Exception {
+        Policy policy = new Policy();
+        policy.setName("name");
+        policy.setPolicyId("123");
+        defaultPolicyService.addPolicy(policy);
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testAddPolicy_nullType_returnBadRequestException() throws Exception {
+        Policy policy = new Policy();
+        policy.setName("name");
+        policy.setPolicyId("123");
+        policy.setBlob("someBlob");
+        defaultPolicyService.addPolicy(policy);
     }
 
     @Test
-    public void testGetPolicy() throws Exception {
+    public void testAddPolicy_validPolicy_callsAddPolicyDAO() throws Exception {
+        Policy policy = new Policy();
+        policy.setName("name");
+        policy.setPolicyId("123");
+        policy.setBlob("someBlob");
+        policy.setPolicyType("someType");
+        defaultPolicyService.addPolicy(policy);
+        verify(policyDao).addPolicy(policy);
+    }
 
+    @Test
+    public void testGetPolicy_validPolicyId() throws Exception {
+        Policy policy = new Policy();
+        policy.setPolicyId("1");
+        policy.setName("name");
+        policy.setBlob("someBlob");
+        policy.setPolicyType("someType");
+        when(policyDao.getPolicy(anyString())).thenReturn(policy);
+        Policy getPolicy = defaultPolicyService.getPolicy("1");
+        assertThat("Check return policy", getPolicy.getName(), equalTo("name"));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetPolicy_invalidPolicyId() throws Exception {
+        when(policyDao.getPolicy(anyString())).thenReturn(null);
+        Policy getPolicy = defaultPolicyService.getPolicy("1");
+        assertThat("Check return policy", getPolicy.getName(), equalTo("name"));
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testUpdatePolicy_nullPolicy() throws Exception {
+        defaultPolicyService.updatePolicy(null,"1");
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testUpdatePolicy_nullName() throws Exception {
+        Policy policy = new Policy();
+        policy.setPolicyId("1");
+        policy.setBlob("someBlob");
+        policy.setPolicyType("someType");
+        defaultPolicyService.updatePolicy(policy,"1");
     }
 
     @Test
     public void testUpdatePolicy() throws Exception {
+        Policy policy = new Policy();
+        policy.setPolicyId("1");
+        policy.setBlob("someBlob");
+        policy.setPolicyType("someType");
+        policy.setName("name");
+        defaultPolicyService.updatePolicy(policy,"1");
+        verify(policyDao).updatePolicy(policy,"1");
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeletePolicy_nullId() throws Exception {
+        defaultPolicyService.deletePolicy(null);
 
     }
 
     @Test
     public void testDeletePolicy() throws Exception {
+        Policy policy = new Policy();
+        policy.setPolicyId("1");
+        policy.setName("name");
+        policy.setBlob("someBlob");
+        policy.setPolicyType("someType");
+        when(policyDao.getPolicy(anyString())).thenReturn(policy);
+        defaultPolicyService.deletePolicy("1");
+        verify(policyDao).deletePolicy(policy.getPolicyId());
 
     }
 }
