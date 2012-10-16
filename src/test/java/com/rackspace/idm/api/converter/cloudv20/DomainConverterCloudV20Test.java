15be6c8ec8ef11db454f2995e540893ca161ccd2
Matt Colton <matt.colton@rackspace.com>
2012-10-16 08:23:25 -0500

    Added Domains test

--- src/test/java/com/rackspace/idm/api/converter/cloudv20/DomainConverterCloudV20Test.java
+++ src/test/java/com/rackspace/idm/api/converter/cloudv20/DomainConverterCloudV20Test.java
@@ -1,6 +1,7 @@
 package com.rackspace.idm.api.converter.cloudv20;
 
 import com.rackspace.docs.identity.api.ext.rax_auth.v1.Domain;
+import com.rackspace.docs.identity.api.ext.rax_auth.v1.Domains;
 import com.rackspace.idm.api.resource.cloud.JAXBObjectFactories;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,13 +18,16 @@ import static org.junit.Assert.assertThat;
  */
 public class DomainConverterCloudV20Test {
     private DomainConverterCloudV20 domainConverterCloudV20;
+    private DomainsConverterCloudV20 domainsConverterCloudV20;
     private JAXBObjectFactories jaxbObjectFactories;
 
     @Before
     public void setUp() throws Exception {
         domainConverterCloudV20 = new DomainConverterCloudV20();
+        domainsConverterCloudV20 = new DomainsConverterCloudV20();
         jaxbObjectFactories = new JAXBObjectFactories();
         domainConverterCloudV20.setObjFactories(jaxbObjectFactories);
+        domainsConverterCloudV20.setObjFactories(jaxbObjectFactories);
     }
 
     @Test
@@ -54,4 +58,17 @@ public class DomainConverterCloudV20Test {
         assertThat("enabled", domainDO.isEnabled(), equalTo(true));
     }
 
+    @Test
+    public void toDomainsDO_returnsDomainsDO() throws Exception {
+        domainsConverterCloudV20.setDomainConverter(domainConverterCloudV20);
+        Domains domains = new Domains();
+        Domain domain = new Domain();
+        domain.setId("domainId");
+        domain.setName("name");
+        domain.setDescription("description");
+        domain.setEnabled(true);
+        domains.getDomain().add(domain);
+        com.rackspace.idm.domain.entity.Domains domainsDO = domainsConverterCloudV20.toDomainsDO(domains);
+        assertThat("size", domainsDO.getDomain().size(), equalTo(1));
+    }
 }
