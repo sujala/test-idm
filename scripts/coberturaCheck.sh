a300409b0a361f4f22e8f84cc691d84708bf81af
Jorge Munoz <jorge.munoz@rackspace.com>
2012-10-17 16:32:26 -0500

    Added idm.properties.location to the coberturacheck

--- scripts/coberturaCheck.sh
+++ scripts/coberturaCheck.sh
@@ -3,4 +3,4 @@
 BASEDIR=$(dirname $(readlink -f $0))
 cd $BASEDIR
 cd ..
-mvn cobertura:cobertura cobertura:check -Dskip.default.surefire=false -Dcobertura.skip.check=false
+mvn cobertura:cobertura cobertura:check -Dskip.default.surefire=false -Dcobertura.skip.check=false -Didm.properties.location=target/config/JENKINS
