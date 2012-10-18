a34107938e4b8a2b0b31d9cdf5a5d5a969c36fdf
Jorge Munoz <jorge.munoz@rackspace.com>
2012-10-18 15:50:08 -0500

    Added more test for user operations

--- src/test/groovy/com/rackspace/idm/api/resource/cloud/v20/Cloud20IntegrationTest.groovy
+++ src/test/groovy/com/rackspace/idm/api/resource/cloud/v20/Cloud20IntegrationTest.groovy
@@ -18,29 +18,31 @@ class Cloud20IntegrationTest extends Specification {
 
     @Shared def path = "cloud/v2.0/"
     @Shared def serviceAdminToken
+    @Shared def identityAdminToken
 
     def randomness = UUID.randomUUID()
-    static def X_AUTH_TOKEN="X-Auth-Token"
+    static def X_AUTH_TOKEN = "X-Auth-Token"
 
     def setupSpec() {
         this.resource = ensureGrizzlyStarted("classpath:app-config.xml");
-        serviceAdminToken = authenticate("authQE","Auth1234").getEntity(AuthenticateResponse).value.token.id
+        serviceAdminToken = authenticate("authQE", "Auth1234").getEntity(AuthenticateResponse).value.token.id
+        identityAdminToken = authenticate("auth", "auth123").getEntity(AuthenticateResponse).value.token.id
     }
 
     def cleanupSpec() {
     }
 
-    def 'User CRUD'(){
+    def 'User CRUD'() {
         when:
         //Create user
-        def randmon = ("$randomness").replace('-',"")
-        def user = identityAdminUser("bob"+randmon,"test@rackspace.com", true, "Password1","ORD")
+        def randmon = ("$randomness").replace('-', "")
+        def user = userForCreate("bob" + randmon, "displayName", "test@rackspace.com", true, "ORD", null, "Password1")
         def response = createUser(serviceAdminToken, user)
         //Get user
         def getUserResponse = getUser(serviceAdminToken, response.location)
         def userEntity = getUserResponse.getEntity(User)
         //Update User
-        def userForUpdate = userForUpdate("updatedBob"+randmon, "Bob", "test@rackspace.com", true, null, null)
+        def userForUpdate = userForUpdate("updatedBob" + randmon, "Bob", "test@rackspace.com", true, null, null)
         def updateUserResponse = updateUser(serviceAdminToken, userEntity.getId(), userForUpdate)
         //Delete user
         def deleteResponses = deleteUser(serviceAdminToken, userEntity.getId())
@@ -56,7 +58,7 @@ class Cloud20IntegrationTest extends Specification {
         hardDeleteResponses.status == 204
     }
 
-    def "operations on non-existent users return 'not found'" () {
+    def "operations on non-existent users return 'not found'"() {
         expect:
         response.status == 404
 
@@ -68,48 +70,71 @@ class Cloud20IntegrationTest extends Specification {
         ]
     }
 
+    def "invalid operations on create user returns 'bad request'"() {
+        expect:
+        response.status == 400
 
-
-    def createUser(String token, user){
-        resource.path(path).path('users').header(X_AUTH_TOKEN,token).entity(user).post(ClientResponse)
+        where:
+        response << [
+                createUser(serviceAdminToken, userForCreate("!@#What", "display", "test@rackspace.com", true, "ORD", null, "Password1")),
+                createUser(serviceAdminToken, userForCreate("1one", "display", "test@rackspace.com", true, "ORD", null, "Password1")),
+                createUser(serviceAdminToken, userForCreate("one name", "display", "test@rackspace.com", true, "ORD", null, "Password1")),
+                createUser(serviceAdminToken, userForCreate("", "display", "test@rackspace.com", true, "ORD", null, "Password1")),
+                createUser(serviceAdminToken, userForCreate(null, "display", "test@rackspace.com", true, "ORD", null, "Password1")),
+                createUser(serviceAdminToken, userForCreate("goodName", "display", "junk!@#", true, "ORD", null, "Password1")),
+                createUser(serviceAdminToken, userForCreate("goodName", "display", "test@rackspace.com", true, "ORD", null, "Pop1")),
+                createUser(serviceAdminToken, userForCreate("goodName", "display", "test@rackspace.com", true, "ORD", null, "longpassword1")),
+                createUser(serviceAdminToken, userForCreate("goodName", "display", "test@rackspace.com", true, "ORD", null, "Longpassword")),
+                createUser(serviceAdminToken, userForCreate("goodName", "display", "test@rackspace.com", true, "ORD", "someId", "Longpassword")),
+                createUser(identityAdminToken, userForCreate("goodName", "display", "test@rackspace.com", true, "ORD", null, "Longpassword1"))
+        ]
     }
 
-    def getUser(String token, URI location){
-        resource.uri(location).accept(APPLICATION_XML).header(X_AUTH_TOKEN,token).get(ClientResponse)
+    def 'operations with invalid tokens'(){
+        expect:
+        response.status == 401
+
+        where:
+        response << [
+                createUser("invalidToken", userForCreate("someName", "display", "test@rackspace.com", true, "ORD", null, "Password1")),
+                getUserById("invalidToken", "badId"),
+                updateUser("invalidToken", "badId", new User()),
+                deleteUser("invalidToken", "badId")
+        ]
+
     }
 
-    def getUserById(String token, String userId){
-        resource.path(path).path('users').path(userId).accept(APPLICATION_XML).header(X_AUTH_TOKEN,token).get(ClientResponse)
+
+
+    def createUser(String token, user) {
+        resource.path(path).path('users').header(X_AUTH_TOKEN, token).entity(user).post(ClientResponse)
     }
 
-    def updateUser(String token, String userId, user){
-        resource.path(path).path('users').path(userId).header(X_AUTH_TOKEN,token).entity(user).post(ClientResponse)
+    def getUser(String token, URI location) {
+        resource.uri(location).accept(APPLICATION_XML).header(X_AUTH_TOKEN, token).get(ClientResponse)
     }
 
-    def deleteUser(String token, String userId){
-        resource.path(path).path('users').path(userId).header(X_AUTH_TOKEN,token).delete(ClientResponse)
+    def getUserById(String token, String userId) {
+        resource.path(path).path('users').path(userId).accept(APPLICATION_XML).header(X_AUTH_TOKEN, token).get(ClientResponse)
     }
 
-    def hardDeleteUser(String token, String userId){
-        resource.path(path).path('softDeleted').path('users').path(userId).header(X_AUTH_TOKEN,token).delete(ClientResponse)
+    def updateUser(String token, String userId, user) {
+        resource.path(path).path('users').path(userId).header(X_AUTH_TOKEN, token).entity(user).post(ClientResponse)
     }
 
-    def identityAdminUser(String username, String email, Boolean enabled, String password, String defaultRegion){
-        new User().with {
-            it.username = username
-            it.email = email
-            it.enabled = enabled
-            it.otherAttributes.put(new QName("http://docs.openstack.org/identity/api/ext/OS-KSADM/v1.0", "password"),password)
-            it.otherAttributes.put(new QName("http://docs.rackspace.com/identity/api/ext/RAX-AUTH/v1.0","defaultRegion"),defaultRegion)
-            return it
-        }
+    def deleteUser(String token, String userId) {
+        resource.path(path).path('users').path(userId).header(X_AUTH_TOKEN, token).delete(ClientResponse)
     }
 
-    def authenticate(username, password){
-        resource.path(path + 'tokens').accept(APPLICATION_XML).entity(authenticateRequest(username,password)).post(ClientResponse)
+    def hardDeleteUser(String token, String userId) {
+        resource.path(path).path('softDeleted').path('users').path(userId).header(X_AUTH_TOKEN, token).delete(ClientResponse)
     }
 
-    def getCredentials(String username, String password){
+    def authenticate(username, password) {
+        resource.path(path + 'tokens').accept(APPLICATION_XML).entity(authenticateRequest(username, password)).post(ClientResponse)
+    }
+
+    def getCredentials(String username, String password) {
         new PasswordCredentialsRequiredUsername().with {
             it.username = username
             it.password = password
@@ -117,18 +142,18 @@ class Cloud20IntegrationTest extends Specification {
         }
     }
 
-    def authenticateRequest(String username, String password){
+    def authenticateRequest(String username, String password) {
         def credentials = getCredentials(username, password)
 
         def objectFactory = new ObjectFactory()
 
-        new AuthenticationRequest().with{
+        new AuthenticationRequest().with {
             it.setCredential(objectFactory.createPasswordCredentials(credentials))
             return it
         }
     }
 
-    def userForUpdate(String username,String displayName, String email, Boolean enabled, String defaultRegion, String password ){
+    def userForUpdate(String username, String displayName, String email, Boolean enabled, String defaultRegion, String password) {
         new User().with {
             it.username = username
             it.email = email
@@ -139,6 +164,25 @@ class Cloud20IntegrationTest extends Specification {
             }
             if (defaultRegion != null) {
                 it.otherAttributes.put(new QName("http://docs.rackspace.com/identity/api/ext/RAX-AUTH/v1.0", "defaultRegion"), defaultRegion)
+            }
+            return it
+        }
+    }
+
+    def userForCreate(String username, String displayName, String email, Boolean enabled, String defaultRegion, String domainId, String password) {
+        new User().with {
+            it.username = (username != null) ? username : null
+            it.displayName = (displayName != null) ? displayName : null
+            it.email = (email != null) ? email : null
+            it.enabled = (enabled != null) ? enabled : null
+            if (defaultRegion != null) {
+                it.otherAttributes.put(new QName("http://docs.rackspace.com/identity/api/ext/RAX-AUTH/v1.0", "defaultRegion"), defaultRegion)
+            }
+            if (domainId != null) {
+                it.otherAttributes.put(new QName("http://docs.rackspace.com/identity/api/ext/RAX-AUTH/v1.0","domainId"), domainId)
+            }
+            if (password != null) {
+                it.otherAttributes.put(new QName("http://docs.openstack.org/identity/api/ext/OS-KSADM/v1.0", "password"), password)
             }
             return it
         }
