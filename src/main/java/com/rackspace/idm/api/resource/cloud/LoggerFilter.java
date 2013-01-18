58b6ec1339b6ab2a7d8fb2b99754db49c196ae2b
werner mendizabal <werner.mendizabal@rackspace.com>
2013-01-18 11:22:29 -0600

    merge v1.0.12.3 branch

--- src/main/java/com/rackspace/idm/api/resource/cloud/LoggerFilter.java
+++ src/main/java/com/rackspace/idm/api/resource/cloud/LoggerFilter.java
@@ -1,5 +1,6 @@
 package com.rackspace.idm.api.resource.cloud;
 
+import com.rackspace.idm.api.filter.MultiReadHttpServletRequest;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
@@ -8,6 +9,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -32,24 +34,26 @@ public class LoggerFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String path = ((HttpServletRequestWrapper) request).getPathInfo();
-        String method = ((HttpServletRequestWrapper) request).getMethod();
-        String host = ((HttpServletRequestWrapper) request).getHeader("Host");
-        String remoteHost = ((HttpServletRequestWrapper) request).getRemoteHost();
-        String userAgent = ((HttpServletRequestWrapper) request).getHeader("User-Agent");
-        String authToken = ((HttpServletRequestWrapper) request).getHeader("X-Auth-Token");
-        String basicAuth = ((HttpServletRequestWrapper) request).getHeader("Authorization");
-        InputStream stream = request.getInputStream();
+        MultiReadHttpServletRequest requestWrapper = new MultiReadHttpServletRequest((HttpServletRequest)request);
+
+        String path = ((HttpServletRequestWrapper) requestWrapper).getPathInfo();
+        String method = ((HttpServletRequestWrapper) requestWrapper).getMethod();
+        String host = ((HttpServletRequestWrapper) requestWrapper).getHeader("Host");
+        String remoteHost = ((HttpServletRequestWrapper) requestWrapper).getRemoteHost();
+        String userAgent = ((HttpServletRequestWrapper) requestWrapper).getHeader("User-Agent");
+        String authToken = ((HttpServletRequestWrapper) requestWrapper).getHeader("X-Auth-Token");
+        String basicAuth = ((HttpServletRequestWrapper) requestWrapper).getHeader("Authorization");
+        InputStream stream = requestWrapper.getInputStream();
         String requestBody = IOUtils.toString(stream);
-        String requestType = ((HttpServletRequestWrapper) request).getHeader("Content-Type");
+        String requestType = ((HttpServletRequestWrapper) requestWrapper).getHeader("Content-Type");
         Long startTime = new Date().getTime();
 
         StatusExposingServletResponse responseWrapper = new StatusExposingServletResponse((HttpServletResponse)response);
 
-        chain.doFilter(request, responseWrapper);
+        chain.doFilter(requestWrapper, responseWrapper);
         int status = responseWrapper.getStatus();
         String responseBody = responseWrapper.getBody();
-        String responseType = ((HttpServletRequestWrapper) request).getHeader("Accept");
+        String responseType = ((HttpServletRequestWrapper) requestWrapper).getHeader("Accept");
 
         // TODO: make async
         analyticsLogger.log
---
+++ src/test/groovy/com/rackspace/idm/api/resource/cloud/v20/DelegateCloud20ServiceAuthGroovyTest.groovy
@@ -0,0 +1,443 @@
+package com.rackspace.idm.api.resource.cloud.v20
+
+import com.rackspace.idm.api.resource.cloud.CloudClient
+import com.rackspace.idm.domain.entity.ScopeAccess
+import com.rackspace.idm.domain.service.ScopeAccessService
+import com.rackspace.idm.exception.NotAuthorizedException
+import com.rackspace.idm.exception.NotFoundException
+import com.rackspace.idm.validation.Validator20
+import org.apache.commons.configuration.Configuration
+import spock.lang.Shared
+import spock.lang.Specification
+
+import javax.ws.rs.core.Response
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jorge
+ * Date: 1/14/13
+ * Time: 4:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class DelegateCloud20ServiceAuthGroovyTest extends Specification{
+    @Shared DelegateCloud20Service delegateCloud20Service
+    @Shared Validator20 validator20
+    @Shared ScopeAccessService scopeAccessService
+    @Shared DefaultCloud20Service defaultCloud20Service
+    @Shared CloudClient cloudClient
+    @Shared Configuration config
+    @Shared expiredDate
+    @Shared futureDate
+
+
+    def setupSpec(){
+        delegateCloud20Service = new DelegateCloud20Service()
+        expiredDate = new Date().minus(5)
+        futureDate = new Date().plus(5)
+    }
+
+    def setup(){
+        setupMocks()
+        config.getBoolean("useCloudAuth") >> "true"
+    }
+
+    def "Service token: CA - not found or expired & GA - not found | User token: CA - Not Found, expired or valid  & GA - Not found, expired, or valid"(){
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def builtResponse = response.build()
+        builtResponse.status == 401
+    }
+
+    def "Service token: CA - not found or expired & GA - not found | User token: CA - not found, expired, or valid & GA - expired"(){
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [null, createScopeAccess(expiredDate)]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def builtResponse = response.build()
+        builtResponse.status == 401
+    }
+
+    def "Service token: CA - not found or expired & GA - not found | User token: CA - not found, expired, or valid & GA - valid"(){
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [null, createScopeAccess(futureDate)]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def builtResponse = response.build()
+        builtResponse.status == 401
+    }
+
+    def "Service token: CA - valid & GA - not found | User token: CA - not found, expired & GA - not found"(){
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(404)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [null, null]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def builtResponse = response.build()
+        builtResponse.status == 404
+    }
+
+    def "Service token: CA - valid & GA - not found | User token: CA - valid & GA - not found"(){
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [null, null]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def builtResponse = response.build()
+        builtResponse.status == 200
+    }
+
+    def "Service token: CA - valid & GA - not found | User token: CA - valid & GA - expired"(){
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [null, createScopeAccess(expiredDate)]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotFoundException)
+    }
+
+    def "Service token: CA - valid & GA - not found | User token: CA - not found or expired & GA - valid"(){
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(404)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [null, createScopeAccess(expiredDate)]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def builtResponse = response.build()
+        builtResponse.status == 404
+    }
+
+    def "Service token: CA - valid & GA - not found | User token: CA - valid & GA - valid"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [null, createScopeAccess(futureDate)]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def builtResponse = response.build()
+        builtResponse.status == 200
+    }
+
+    def "Service token: CA - not found - & GA - expired | User token: CA - not found, expired or valid & GA - not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), null]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - not found - & GA - expired | User token: CA - not found, expired or valid & GA - expired"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), createScopeAccess(expiredDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - not found - & GA - expired | User token: CA - not found, expired or valid & GA - valid"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), createScopeAccess(futureDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - expired & GA - expired | User token: CA - not found, expired or valid & GA - not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), null]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - expired & GA - expired | User token: CA - not found, expired or valid & GA - expired"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), createScopeAccess(expiredDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - expired & GA - expired | User token: CA - not found, expired or valid & GA -valid"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), createScopeAccess(futureDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA valid - & GA - expired | User token: CA - not found or expired & GA - not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(404)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), null]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - valid & GA - expired | User token: CA valid - & GA - not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), null]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - valid & GA - expired | User token: CA not found or expired - & GA - expired "() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(404)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), createScopeAccess(expiredDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - valid & GA - expired | User token: CA - valid & GA - future"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(expiredDate), createScopeAccess(futureDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotAuthorizedException)
+    }
+
+    def "Service token: CA - not found & GA - valid | User token: CA - not found, expired, valid & GA - not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), null]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotFoundException)
+
+    }
+
+    def "Service token: CA - not found & GA - valid | User token: CA - not found, expired, valid & GA - expired"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(expiredDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotFoundException)
+    }
+
+    def "Service token: CA - not found & GA - valid | User token: CA - not found, expired or valid & GA - valid"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(futureDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        1 * defaultCloud20Service.validateToken(_, _, _, _)
+    }
+
+    def "Service token: CA - expired & GA - valid | User token: CA - not found, expired, or valid & GA - not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), null]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotFoundException)
+    }
+
+    def "Service token: CA - expired & GA - valid | User token: CA - not found, expired, or valid & GA - expired"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(expiredDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotFoundException)
+    }
+
+    def "Service token: CA - expired & GA - valid | User token: CA - not found, expired, or valid & GA - valid"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(401)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(futureDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        1 * defaultCloud20Service.validateToken(_, _, _, _)
+    }
+
+    def "Service token: CA - valid & GA - valid | User token: CA - not found, expired & GA - not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(404)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), null]
+
+        when:
+        def response = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def responseBuilder = response.build()
+        responseBuilder.status == 404
+    }
+
+    def "Service token: CA - valid & GA - valid | User token: CA - valid & GA not found"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), null]
+
+        when:
+        def builder = delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        def response = builder.build()
+        response.status == 200
+
+
+    }
+
+    def "Service token: CA - valid & GA - valid | User token: CA - not found or expired & GA - expired" () {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(404)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(expiredDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotFoundException)
+    }
+
+    def "Service token: CA - valid & GA - valid | User token: CA - valid & GA - expired"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(expiredDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        thrown(NotFoundException)
+    }
+
+    def "Service token: CA - valid & GA - valid | User token: CA - not found or expired & GA - valid"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(404)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(futureDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        1 * defaultCloud20Service.validateToken(_, _, _, _)
+    }
+
+    def "Service token: CA - valid & GA - valid | User token: CA - valid & GA - valid"() {
+        given:
+        cloudClient.get(_,_) >> createCloudAuthGetResponse(200)
+        scopeAccessService.getScopeAccessByAccessToken(_) >>> [createScopeAccess(futureDate), createScopeAccess(futureDate)]
+
+        when:
+        delegateCloud20Service.validateToken(null, "token1", "token2", null)
+
+        then:
+        1 * defaultCloud20Service.validateToken(_, _, _, _)
+    }
+
+
+
+    def createScopeAccess(Date expTime) {
+        new ScopeAccess().with {
+            it.accessTokenExp = expTime
+            it.accessTokenString = "tokenString"
+            return it
+        }
+    }
+
+    def createCloudAuthGetResponse(int status){
+        Response.ResponseBuilder responseBuilder = Response.status(status)
+        return responseBuilder
+    }
+
+    def setupMocks(){
+        validator20 = Mock()
+        delegateCloud20Service.validator20 = validator20
+        scopeAccessService = Mock()
+        delegateCloud20Service.scopeAccessService = scopeAccessService
+        cloudClient = Mock()
+        delegateCloud20Service.cloudClient = cloudClient
+        config = Mock()
+        delegateCloud20Service.config = config
+        defaultCloud20Service = Mock()
+        delegateCloud20Service.defaultCloud20Service = defaultCloud20Service
+
+    }
+}
