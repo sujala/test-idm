1391037f4829b3a003ffa5c2bd8fba74cb04259d
werner mendizabal <werner.mendizabal@rackspace.com>
2013-01-21 14:39:22 -0600

    do not deploy to test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,29 +4,11 @@ BASEDIR=$(dirname $(readlink -f $0))
 
 if [ $# -ne 1 ]
 then
-    echo "usage: $0 <test1|test2|dev1|dev2>"
+    echo "usage: $0 <dev1|dev2>"
     exit
 fi
 
 DEPLOY="$1"
-
-if [ $DEPLOY == "test1" ]
-then
-    HOST=t-api1.cidm.iad2.corp.rackspace.com
-
-    $BASEDIR/editProperties.sh useCloudAuth=false useCloudAuth=true
-    $BASEDIR/editProperties.sh gaIsSourceOfTruth=true gaIsSourceOfTruth=false
-    $BASEDIR/copyProperties.sh $HOST TEST
-fi
-
-if [ $DEPLOY == "test2" ]
-then
-    HOST=t-api2.cidm.iad2.corp.rackspace.com
-
-    $BASEDIR/editProperties.sh useCloudAuth=true useCloudAuth=false
-    $BASEDIR/editProperties.sh gaIsSourceOfTruth=false gaIsSourceOfTruth=true
-    $BASEDIR/copyProperties.sh $HOST TEST
-fi
 
 if [ $DEPLOY == "dev1" ]
 then
