e1172e2016b74720a93fa52169d9f71287b8e388
Jorge Munoz <jorge.munoz@rackspace.com>
2013-02-07 11:48:30 -0600

    Added base directory path to jmeter script

--- scripts/runJmeter.sh
+++ scripts/runJmeter.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+BASEDIR=$(dirname $(readlink -f $0))
+
 echo "### Running JMeter performance test ###"
 
 # Clear out old results
-rm ./jmeter/results.jtl
+rm $BASEDIR/jmeter/results.jtl
 
 # Run the tests
 echo "## Running the tests"
-jmeter -n -t ./jmeter/AuthValidate_JenkinsUseOnly.jmx -l ./jmeter/results.jtl
+jmeter -n -t $BASEDIR/jmeter/AuthValidate_JenkinsUseOnly.jmx -l $BASEDIR/jmeter/results.jtl
