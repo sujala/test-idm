660443da22eff13c75ca8f08616175f468416149
Jorge Munoz <jorge.munoz@rackspace.com>
2013-02-11 14:42:59 -0600

    Added user.properties file for runJmeter sh

--- scripts/runJmeter.sh
+++ scripts/runJmeter.sh
@@ -8,7 +8,8 @@ echo "### Running JMeter performance test ###"
 
 # Clear out old results
 rm $BASEDIR/jmeter/results.jtl
+rm $BASEDIR/jmeter.log
 
 # Run the tests
 echo "## Running the tests"
-jmeter -n -t $BASEDIR/jmeter/AuthValidate_JenkinsUseOnly.jmx -l $BASEDIR/jmeter/results.jtl
+jmeter -n -p $BASEDIR/jmeter/user.properties -t $BASEDIR/jmeter/AuthValidate_JenkinsUseOnly.jmx -l $BASEDIR/jmeter/results.jtl
