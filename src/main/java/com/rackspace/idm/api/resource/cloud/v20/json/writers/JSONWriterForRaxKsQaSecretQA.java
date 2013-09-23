862e02357bfd3edf7e85ce104e3d5fc7fe954cbc
Matt Kovacs <matt.kovacs@rackspace.com>
2013-09-23 10:50:32 -0500

    Adding Provider to new JSONWriter

--- src/main/java/com/rackspace/idm/api/resource/cloud/v20/json/writers/JSONWriterForRaxKsQaSecretQA.java
+++ src/main/java/com/rackspace/idm/api/resource/cloud/v20/json/writers/JSONWriterForRaxKsQaSecretQA.java
@@ -3,9 +3,11 @@ package com.rackspace.idm.api.resource.cloud.v20.json.writers;
 import com.rackspace.docs.identity.api.ext.rax_ksgrp.v1.Group;
 import com.rackspace.docs.identity.api.ext.rax_ksqa.v1.SecretQA;
 
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
@@ -15,6 +17,8 @@ import java.util.LinkedHashMap;
 
 import static com.rackspace.idm.JSONConstants.*;
 
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
 public class JSONWriterForRaxKsQaSecretQA extends JSONWriterForEntity<SecretQA>  {
     @Override
     public void writeTo(SecretQA secretQA, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
