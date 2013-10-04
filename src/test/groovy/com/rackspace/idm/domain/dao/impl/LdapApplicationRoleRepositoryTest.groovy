27ca3dc6d2a64862eedeae743535bca27aed32fb
werner mendizabal <werner.mendizabal@rackspace.com>
2013-10-04 10:11:44 -0500

    Unable to Paginate List Roles API Call

--- src/test/groovy/com/rackspace/idm/domain/dao/impl/LdapApplicationRoleRepositoryTest.groovy
+++ src/test/groovy/com/rackspace/idm/domain/dao/impl/LdapApplicationRoleRepositoryTest.groovy
@@ -19,11 +19,11 @@ class LdapApplicationRoleRepositoryTest extends Specification {
     @Shared Configuration config
     @Shared LdapApplicationRoleRepository repo
 
-    @Shared defaultWeightFilterString = "(&(objectClass=clientRole)(|(rsWeight=2000)))"
-    @Shared userAdminWeightFilterString = "(&(objectClass=clientRole)(|(rsWeight=2000)(rsWeight=1000)))"
-    @Shared specialWeightFilterString = "(&(objectClass=clientRole)(|(rsWeight=2000)(rsWeight=1000)(rsWeight=500)))"
-    @Shared adminWeightFilterString =  "(&(objectClass=clientRole)(|(rsWeight=2000)(rsWeight=1000)(rsWeight=500)(rsWeight=100)))"
-    @Shared serviceAdminWeightFilterString = "(&(objectClass=clientRole)(|(rsWeight=2000)(rsWeight=1000)(rsWeight=500)(rsWeight=100)(rsWeight=0)))"
+    @Shared defaultWeightFilterString = "(&(objectClass=clientRole)(rsWeight>=2000))"
+    @Shared userAdminWeightFilterString = "(&(objectClass=clientRole)(rsWeight>=1000))"
+    @Shared specialWeightFilterString = "(&(objectClass=clientRole)(rsWeight>=500))"
+    @Shared adminWeightFilterString = "(&(objectClass=clientRole)(rsWeight>=100))"
+    @Shared serviceAdminWeightFilterString = "(&(objectClass=clientRole)(rsWeight>=0))"
 
     def setupSpec() {
         repo = new LdapApplicationRoleRepository()
@@ -39,19 +39,6 @@ class LdapApplicationRoleRepositoryTest extends Specification {
         config.getInt("cloudAuth.special.rsWeight") >> 500
         config.getInt("cloudAuth.admin.rsWeight") >> 100
         config.getInt("cloudAuth.serviceAdmin.rsWeight") >> 0
-    }
-
-    def "getting roles weights returns all default available weights"() {
-        when:
-        def weights = repo.getRoleWeights()
-
-        then:
-        weights.size() == 5
-        weights.contains(2000)
-        weights.contains(1000)
-        weights.contains(500)
-        weights.contains(100)
-        weights.contains(0)
     }
 
     def "searchFilter_availableClientRoles returns appropriate search filter"() {
@@ -70,27 +57,11 @@ class LdapApplicationRoleRepositoryTest extends Specification {
         serviceAdminWeightFilter.toString().equals(serviceAdminWeightFilterString)
     }
 
-    def "getting role weights or filter returns or filter"() {
-        when:
-        def listOne = repo.getRoleWeightsOrFilter(2000)
-        def listTwo = repo.getRoleWeightsOrFilter(1000)
-        def listThree = repo.getRoleWeightsOrFilter(500)
-        def listFour = repo.getRoleWeightsOrFilter(100)
-        def listFive = repo.getRoleWeightsOrFilter(0)
-
-        then:
-        listOne.size() == 1
-        listTwo.size() == 2
-        listThree.size() == 3
-        listFour.size() == 4
-        listFive.size() == 5
-    }
-
     def "searchFilter by applicationId and available returns filter"() {
         when:
         def filter = repo.searchFilter_availableRolesByApplicationId("applicationId", 2000)
 
         then:
-        filter.toString().equals("(&(objectClass=clientRole)(clientId=applicationId)(|(rsWeight=2000)))")
+        filter.toString().equals("(&(objectClass=clientRole)(clientId=applicationId)(rsWeight>=2000))")
     }
 }
