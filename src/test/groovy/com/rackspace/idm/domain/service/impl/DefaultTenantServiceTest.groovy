f9c58eefd5080463e73e35ea2fe80ca06bdf3a5c
Matt Kovacs <matt.kovacs@rackspace.com>
2013-11-19 11:09:29 -0600

    Fixing rebase

--- src/test/groovy/com/rackspace/idm/domain/service/impl/DefaultTenantServiceTest.groovy
+++ src/test/groovy/com/rackspace/idm/domain/service/impl/DefaultTenantServiceTest.groovy
@@ -1,6 +1,7 @@
 package com.rackspace.idm.domain.service.impl
 import com.rackspace.idm.api.resource.cloud.atomHopper.AtomHopperConstants
 import com.rackspace.idm.domain.entity.ClientRole
+import com.rackspace.idm.domain.entity.FederatedToken
 import com.rackspace.idm.domain.entity.ScopeAccess
 import com.rackspace.idm.domain.entity.TenantRole
 import com.rackspace.idm.exception.ClientConflictException
