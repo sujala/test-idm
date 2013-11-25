547642490f032d9750467b4aadbdcc7ce807a9f0
jorge.munoz <jorge.munoz@rackspace.com>
2013-11-25 16:39:44 -0600

    Added test to verify D-16365 and D-16366

--- src/test/groovy/com/rackspace/idm/api/resource/cloud/v20/Cloud20UserIntegrationTest.groovy
+++ src/test/groovy/com/rackspace/idm/api/resource/cloud/v20/Cloud20UserIntegrationTest.groovy
@@ -4,6 +4,7 @@ import org.openstack.docs.identity.api.ext.os_ksadm.v1.UserForCreate
 import spock.lang.Shared
 import testHelpers.RootIntegrationTest
 import static com.rackspace.idm.Constants.DEFAULT_PASSWORD
+import static com.rackspace.idm.Constants.USER_MANAGE_ROLE_ID
 
 /**
  * Created with IntelliJ IDEA
@@ -60,5 +61,61 @@ class Cloud20UserIntegrationTest extends RootIntegrationTest{
         utils.deleteGroup(group)
         utils.deleteUsers(defaultUser, userManage, userAdmin, identityAdmin)
         utils.deleteDomain(domainId)
+    }
+
+    def "Only User admin within the same domain should be allowed to retrieve other user admin's apiKey" () {
+        given:
+        def domainId = utils.createDomain()
+        def domainId2 = utils.createDomain()
+        (identityAdmin, userAdmin, userManage, defaultUser) = utils.createUsers(domainId)
+
+        when:
+        def userAdminTwo = utils.createUser(utils.getIdentityAdminToken(), utils.getRandomUUID("userAdmin2"), domainId2)
+        def userAdminThree = utils.createUser(utils.getIdentityAdminToken(), utils.getRandomUUID("userAdmin3"), domainId2)
+        String token = utils.getToken(userAdmin.username)
+        String userAdminThreeToken = utils.getToken(userAdminThree.username)
+        def credentials = utils.addApiKeyToUser(userAdminTwo)
+        def userAdminThreeResponse = cloud20.getUserApiKey(userAdminThreeToken, userAdminTwo.id)
+        def response = cloud20.getUserApiKey(token, userAdminTwo.id)
+
+        then:
+        credentials.username == userAdminTwo.username
+        userAdminThreeResponse.status == 200
+        response.status == 403
+
+
+        cleanup:
+        utils.deleteUsers(defaultUser, userManage, userAdmin, identityAdmin, userAdminTwo, userAdminThree)
+        utils.deleteDomain(domainId)
+        utils.deleteDomain(domainId2)
+    }
+
+    def "Only user manage within the same domain should be allowed to retrieve other user manage's apiKey" () {
+        given:
+        def domainId = utils.createDomain()
+        def domainId2 = utils.createDomain()
+        (identityAdmin, userAdmin, userManage, defaultUser) = utils.createUsers(domainId)
+
+        when:
+        def userAdminTwo = utils.createUser(utils.getIdentityAdminToken(), utils.getRandomUUID("userAdmin2"), domainId2)
+        def userAdminTwoToken = utils.getToken(userAdminTwo.username)
+        def userManageTwo = utils.createUser(userAdminTwoToken, utils.getRandomUUID("userManage2"), domainId2)
+        def userManageThree = utils.createUser(userAdminTwoToken, utils.getRandomUUID("userManage3"), domainId2)
+        utils.addRoleToUser(userManageThree, USER_MANAGE_ROLE_ID)
+        String userManageThreeToken = utils.getToken(userManageThree.username)
+        String token = utils.getToken(userManage.username)
+        def credentials = utils.addApiKeyToUser(userManageTwo)
+        def userManageThreeResponse = cloud20.getUserApiKey(userManageThreeToken, userManageTwo.id)
+        def response = cloud20.getUserApiKey(token, userManageTwo.id)
+
+        then:
+        credentials.username == userManageTwo.username
+        userManageThreeResponse.status == 200
+        response.status == 403
+
+        cleanup:
+        utils.deleteUsers(defaultUser, userManage, userAdmin, identityAdmin, userManageTwo, userManageThree, userAdminTwo)
+        utils.deleteDomain(domainId)
+        utils.deleteDomain(domainId2)
     }
 }
