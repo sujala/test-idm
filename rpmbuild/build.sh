26e337634400fc17bd7a7ada2174190b98443a52
Brendan ODonnell <brendan.james.odonnell@gmail.com>
2014-02-12 13:31:20 -0600

    Update build artifact dirs

--- rpmbuild/build.sh
+++ rpmbuild/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-war=$(find ../ -maxdepth 1 -name *.war -print)
+war=$(find ../build/libs -maxdepth 1 -name *.war -print)
 version=$(echo $war | grep -oP '\d+\.\d+\.\d+')
 release=$(echo $war | grep -oP '\d{4,}')
 
