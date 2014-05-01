d45f7532b1393490c3360836174db56e61efc8fa
Bob Jacoby <robert.jacoby@rackspace.com>
2014-05-01 16:02:06 -0500

    [B-58104] - Fixing typo in constant and error message

--- src/test/groovy/com/rackspace/idm/domain/service/impl/DefaultFederatedIdentityServiceTest.groovy
+++ src/test/groovy/com/rackspace/idm/domain/service/impl/DefaultFederatedIdentityServiceTest.groovy
@@ -78,7 +78,7 @@ class DefaultFederatedIdentityServiceTest extends Specification {
                 "      </saml2:Subject>\n" +
                 "      <saml2:AuthnStatement AuthnInstant=\"2013-10-01T15:02:42.103Z\">\n" +
                 "         <saml2:AuthnContext>\n" +
-                "            <saml2:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordPotectedTransport</saml2:AuthnContextClassRef>\n" +
+                "            <saml2:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport</saml2:AuthnContextClassRef>\n" +
                 "         </saml2:AuthnContext>\n" +
                 "      </saml2:AuthnStatement>\n" +
                 "      <saml2:AttributeStatement>\n" +
