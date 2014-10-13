fe1827f8a4bb598a19cad564252f062c9c31e130
Timothy Cline <timothy.cline@rackspace.com>
2014-10-13 11:24:17 -0500

    B-79475 - Authenticated Encrypted Token :: Update Validate Token API Service - Support Regular & Impersonation Authenticated Encrypted Tokens

--- src/test/groovy/com/rackspace/idm/api/resource/cloud/v20/Cloud20AEIntegrationTest.groovy
+++ src/test/groovy/com/rackspace/idm/api/resource/cloud/v20/Cloud20AEIntegrationTest.groovy
@@ -4,6 +4,7 @@ import com.rackspace.docs.identity.api.ext.rax_auth.v1.TokenFormatEnum
 import org.springframework.core.io.ClassPathResource
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Shared
+import spock.lang.Unroll
 import testHelpers.RootIntegrationTest
 
 import static com.rackspace.idm.Constants.DEFAULT_PASSWORD
@@ -16,6 +17,12 @@ class Cloud20AEIntegrationTest extends RootIntegrationTest {
 
     @Shared def identityAdmin, userAdmin, userManage, defaultUser, users
     @Shared def domainId
+
+    enum GetTokenVersion {
+        v20,
+        v11,
+        v10
+    }
 
     @Override
     public void doSetupSpec() {
@@ -108,6 +115,71 @@ class Cloud20AEIntegrationTest extends RootIntegrationTest {
         cleanup:
         utils.deleteUsers(users)
         utils.deleteDomain(domainId)
+    }
+
+    @Unroll
+    def "validate AE token X-Auth-Token = #authTokenFormat validateToken = #validateTokenFormat getTokenVersion = #getTokenVersion"() {
+        given:
+        def domainId = utils.createDomain()
+        def identityAdmin, userAdmin, userManage, defaultUser
+        (identityAdmin, userAdmin, userManage, defaultUser) = utils.createUsers(domainId)
+        def users = [defaultUser, userManage, userAdmin, identityAdmin]
+        setUserTokenFormat(identityAdmin, authTokenFormat)
+        setUserTokenFormat(userAdmin, validateTokenFormat)
+        utils.addApiKeyToUser(identityAdmin)
+        utils.addApiKeyToUser(userAdmin)
+        def identityAdminToken = getToken(identityAdmin, getTokenVersion)
+        def userAdminToken = getToken(userAdmin, getTokenVersion)
+
+        when: "v2.0"
+        def response = cloud20.validateToken(identityAdminToken, userAdminToken)
+
+        then:
+        response.status == 200
+
+        when: "v1.1"
+        response = cloud11.validateToken(userAdminToken)
+
+        then:
+        response.status == 200
+
+        cleanup:
+        utils.deleteUsers(users)
+        utils.deleteDomain(domainId)
+
+        where:
+        authTokenFormat      | validateTokenFormat  | getTokenVersion
+        TokenFormatEnum.AE   | TokenFormatEnum.AE   | GetTokenVersion.v20
+        TokenFormatEnum.AE   | TokenFormatEnum.UUID | GetTokenVersion.v20
+        TokenFormatEnum.UUID | TokenFormatEnum.AE   | GetTokenVersion.v20
+        TokenFormatEnum.UUID | TokenFormatEnum.UUID | GetTokenVersion.v20
+
+        TokenFormatEnum.AE   | TokenFormatEnum.AE   | GetTokenVersion.v11
+        TokenFormatEnum.AE   | TokenFormatEnum.UUID | GetTokenVersion.v11
+        TokenFormatEnum.UUID | TokenFormatEnum.AE   | GetTokenVersion.v11
+        TokenFormatEnum.UUID | TokenFormatEnum.UUID | GetTokenVersion.v11
+
+        TokenFormatEnum.AE   | TokenFormatEnum.AE   | GetTokenVersion.v10
+        TokenFormatEnum.AE   | TokenFormatEnum.UUID | GetTokenVersion.v10
+        TokenFormatEnum.UUID | TokenFormatEnum.AE   | GetTokenVersion.v10
+        TokenFormatEnum.UUID | TokenFormatEnum.UUID | GetTokenVersion.v10
+
+    }
+
+    def setUserTokenFormat(user, tokenFormat) {
+        def retrievedUser = utils.getUserById(user.id)
+        retrievedUser.tokenFormat = tokenFormat
+        utils.updateUser(retrievedUser)
+    }
+
+    def getToken(user, version) {
+        if(version == GetTokenVersion.v20) {
+           return utils.getToken(user.username)
+        } else if(version == GetTokenVersion.v11) {
+            return utils11.getToken(user.username)
+        } else {
+            return utils10.getToken(user.username)
+        }
     }
 
 }
--- src/test/groovy/com/rackspace/idm/helpers/Cloud10Utils.groovy
+++ src/test/groovy/com/rackspace/idm/helpers/Cloud10Utils.groovy
@@ -1,10 +1,13 @@
 package com.rackspace.idm.helpers
 
+import com.rackspace.idm.api.resource.cloud.v10.Cloud10VersionResource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import testHelpers.Cloud10Methods
 
 import javax.annotation.PostConstruct
+
+import static com.rackspace.idm.Constants.*
 
 import static org.apache.http.HttpStatus.SC_NO_CONTENT
 
@@ -18,6 +21,11 @@ class Cloud10Utils {
         def response = methods.authenticate(username, key)
         assert (response.status == SC_NO_CONTENT)
         response.headers
+    }
+
+    def getToken(def username, def key = DEFAULT_API_KEY) {
+        def headers = authenticate(username, key)
+        return headers[Cloud10VersionResource.HEADER_AUTH_TOKEN][0]
     }
 
     static def removeTenantFromEndpoint(String endpoint){
