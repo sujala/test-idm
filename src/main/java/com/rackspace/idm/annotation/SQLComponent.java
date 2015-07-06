6304aa7a40b3ec0346f0a538082eab36b72b5664
Bernardo Silva <bernardo.silva@RACKSPACE.COM>
2015-07-06 10:01:34 -0500

    [CIDMDEV-5116] Adding Hibernate support.

---
+++ src/main/java/com/rackspace/idm/annotation/SQLComponent.java
@@ -0,0 +1,16 @@
+package com.rackspace.idm.annotation;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Profile("SQL")
+@Component
+public @interface SQLComponent {
+}
