04f754a809119a806ecac3da896d749a87a2add0
Bernardo Silva <bernardo.silva@RACKSPACE.COM>
2015-07-15 13:21:50 -0500

    [CIDMDEV-5108] - Develop IDM SQL Dao :: Property

---
+++ src/main/java/com/rackspace/idm/domain/sql/mapper/impl/PropertyMapper.java
@@ -0,0 +1,10 @@
+package com.rackspace.idm.domain.sql.mapper.impl;
+
+import com.rackspace.idm.annotation.SQLComponent;
+import com.rackspace.idm.domain.entity.Property;
+import com.rackspace.idm.domain.sql.entity.SqlProperty;
+import com.rackspace.idm.domain.sql.mapper.SqlMapper;
+
+@SQLComponent
+public class PropertyMapper extends SqlMapper<Property, SqlProperty> {
+}
