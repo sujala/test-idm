63f0a968f947119b722d5ecf7a7fe6502089c932
Bernardo Silva <bernardo.silva@RACKSPACE.COM>
2015-07-29 17:09:17 -0500

    [CIDMDEV-4973] Generalizing FederatedUserDao to simplify IdentityUserDao.

--- src/main/java/com/rackspace/idm/domain/dao/FederatedUserDao.java
+++ src/main/java/com/rackspace/idm/domain/dao/FederatedUserDao.java
@@ -6,10 +6,29 @@ import com.rackspace.idm.domain.entity.FederatedUser;
  * Operations for provisioned user based identity providers
  */
 public interface FederatedUserDao extends FederatedBaseUserDao<FederatedUser> {
+
     /**
      * Retrieve all federated users that belong to the specified domain across all identity providers
      * @param domainId
      * @return
      */
     Iterable<FederatedUser> getUsersByDomainId(String domainId);
+
+    /**
+     * Retrieve all federated users that belong to the specified domain and identity provider
+     * @param domainId
+     * @param identityProviderName
+     * @return
+     */
+    Iterable<FederatedUser> getFederatedUsersByDomainIdAndIdentityProviderName(String domainId, String identityProviderName);
+
+
+    /**
+     * Count federated users that belong to the specified domain and identity provider
+     * @param domainId
+     * @param identityProviderName
+     * @return
+     */
+    int getFederatedUsersByDomainIdAndIdentityProviderNameCount(String domainId, String identityProviderName);
+
 }
