a22638a23d537a3ec254dcd0c3cb97d9f6a2df22
Jorge Munoz <jorge.munoz@rackspace.com>
2015-08-10 18:14:12 -0500

    CIDMDEV-5100 - Fix Domain Dao

--- src/main/java/com/rackspace/idm/domain/sql/mapper/impl/DomainMapper.java
+++ src/main/java/com/rackspace/idm/domain/sql/mapper/impl/DomainMapper.java
@@ -4,13 +4,54 @@ import com.rackspace.idm.annotation.SQLComponent;
 import com.rackspace.idm.domain.entity.Domain;
 import com.rackspace.idm.domain.sql.entity.SqlDomain;
 import com.rackspace.idm.domain.sql.entity.SqlDomainRax;
+import com.rackspace.idm.domain.sql.entity.SqlProject;
 import com.rackspace.idm.domain.sql.mapper.SqlRaxMapper;
+import org.apache.xalan.xsltc.DOM;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @SQLComponent
 public class DomainMapper extends SqlRaxMapper<Domain, SqlDomain, SqlDomainRax> {
+
+    @Override
+    public Domain fromSQL(SqlDomain entity) {
+        Domain domain = super.fromSQL(entity);
+
+        if (domain == null){
+            return null;
+        }
+
+        List<String> tenantIds = new ArrayList<String>();
+
+        for (SqlProject sqlProject : entity.getSqlProject()) {
+            tenantIds.add(sqlProject.getTenantId());
+        }
+
+        domain.setTenantIds(tenantIds.toArray((new String[tenantIds.size()])));
+
+        return domain;
+    }
+
+    @Override
+    public SqlDomain toSQL(Domain entity) {
+        SqlDomain sqlDomain = super.toSQL(entity);
+
+        if (sqlDomain == null) {
+            return null;
+        }
+
+        Set<SqlProject> sqlProjects = new HashSet<SqlProject>();
+
+        for (String tenantId : entity.getTenantIds()){
+            SqlProject sqlProject = new SqlProject();
+            sqlProject.setTenantId(tenantId);
+            sqlProjects.add(sqlProject);
+        }
+
+        sqlDomain.setSqlProject(sqlProjects);
+
+        return sqlDomain;
+    }
 
     public List<String> getExtraAttributes() {
         return Arrays.asList("description");
