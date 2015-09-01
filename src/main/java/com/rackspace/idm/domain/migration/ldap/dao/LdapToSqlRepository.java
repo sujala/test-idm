af2df71a7bd0cc469df05a26dcfe2480a93c0606
Bernardo Silva <bernardo.silva@RACKSPACE.COM>
2015-09-01 13:53:19 -0500

    [CIDMDEV-5144] - Creating delta migration profile to make LDAP profile independent of MariaDB.

--- src/main/java/com/rackspace/idm/domain/migration/ldap/dao/LdapToSqlRepository.java
+++ src/main/java/com/rackspace/idm/domain/migration/ldap/dao/LdapToSqlRepository.java
@@ -1,6 +1,6 @@
 package com.rackspace.idm.domain.migration.ldap.dao;
 
-import com.rackspace.idm.annotation.LDAPComponent;
+import com.rackspace.idm.annotation.MigrationComponent;
 import com.rackspace.idm.domain.migration.ldap.entity.LdapToSqlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@LDAPComponent
+@MigrationComponent
 @Repository
 public interface LdapToSqlRepository extends JpaSpecificationExecutor<LdapToSqlEntity>, JpaRepository<LdapToSqlEntity, String> {
     @Query("select c from LdapToSqlEntity c where c.type = :type order by c.created asc")
