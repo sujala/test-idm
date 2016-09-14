387e4d09570df4234cf708606302819809f9e6d6
tokunbo <tokunbo.george@RACKSPACE.COM>
2016-09-14 14:51:47 -0700

    JIRA CID-249 Automated tests for unboundId timeout settings
    - Expose unboundId timeout settings as configuration options
    - Verifying that they are found where expected
    
    --CHANGES--
    API - No
    Config - No
    LDAP - No

---
+++ tests/api/v2/config_props/test_verify_unboundid_timeout_config_visibility.py
@@ -0,0 +1,42 @@
+import ddt
+from tests.api.v2 import base
+from tests.api import constants as const
+from tests.api.v2.schema import unboundid as unboundid_json
+
+
+@ddt.ddt
+class TestListUnboundIdConfigTimeoutSettings(base.TestBaseV2):
+    """
+    List unboundId config
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+        Class level set up for the tests
+        """
+        super(TestListUnboundIdConfigTimeoutSettings, cls).setUpClass()
+
+    def setUp(self):
+        super(TestListUnboundIdConfigTimeoutSettings, self).setUp()
+
+    def test_verify_unboundid_timeout_settings_visibility(self):
+        """
+        Verify unboundid timeout settings visible in config
+        JIRA CID-249 says these must be found in config
+        """
+        if not self.test_config.run_service_admin_tests:
+            self.skipTest('Skipping Service Admin Tests per config value')
+        resp = self.service_admin_client.list_unboundid_config()
+        self.assertEqual(resp.status_code, 200)
+        self.assertSchema(response=resp,
+                          json_schema=unboundid_json.config_list)
+
+        idm_prop_names = [idm_property[const.NAME] for idm_property
+                          in resp.json()[const.IDM_PROPERTIES]]
+        for config_name in const.EXPECTED_UNBOUNDID_TIMEOUT_CONFIGS:
+            self.assertTrue(config_name in idm_prop_names,
+                            msg="Cannot find {0} in idm.properties".format(
+                                config_name))
+
+    def tearDown(self):
+        super(TestListUnboundIdConfigTimeoutSettings, self).tearDown()
