c7111f07715f8c028754b337bcf72d99e6a071d0
tokunbo <tokunbo.george@RACKSPACE.COM>
2016-10-14 13:04:53 -0700

    CID-453 Add Tenant Type to tenant tests in Johny
    --CHANGES--
    API - No
    Config - No
    LDAP - No

--- tests/api/v2/tenants/test_add_tenant.py
+++ tests/api/v2/tenants/test_add_tenant.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*
 import copy
-
 import ddt
 
 from tests.api import constants as const
@@ -12,15 +11,69 @@ from tests.api.v2.schema import tenants
 
 @ddt.ddt
 class TestAddTenant(base.TestBaseV2):
-
     """Add Tenant Tests"""
-
     def setUp(self):
         super(TestAddTenant, self).setUp()
         self.tenant_ids = []
 
+    def test_create_tenant_with_types(self):
+        tenant_description = 'A tenant described'
+        tenant_display_name = 'A name displayed'
+        tenant_type_1 = self.generate_random_string(
+            pattern=const.TENANT_TYPE_PATTERN).upper()
+        tenant_type_2 = self.generate_random_string(
+            pattern=const.TENANT_TYPE_PATTERN).upper()
+        tenant_type_3 = self.generate_random_string(
+            pattern=const.TENANT_TYPE_PATTERN).upper()
+        add_tenant_with_types_schema = copy.deepcopy(tenants.add_tenant)
+        (add_tenant_with_types_schema['properties'][const.TENANT]
+            ['properties'].update({const.NS_TYPES: {}}))
+        (add_tenant_with_types_schema['properties'][const.TENANT]
+            ['required'].append(const.NS_TYPES))
+        tenant_name = tenant_id = 'tname{0}'.format(
+            self.generate_random_string(const.LOWER_CASE_LETTERS))
+        request_object = requests.Tenant(
+                tenant_name=tenant_name,
+                description=tenant_description,
+                tenant_id=tenant_id,
+                enabled=True,
+                tenant_types=[tenant_type_1,
+                              tenant_type_2,
+                              tenant_type_3],
+                display_name=tenant_display_name)
+        resp = self.identity_admin_client.add_tenant(tenant=request_object)
+        self.assertEqual(resp.status_code, 201)
+        self.assertSchema(response=resp,
+                          json_schema=add_tenant_with_types_schema)
+        self.assertEqual(
+            len(resp.json()[const.TENANT][const.NS_TYPES]), 3)
+        for tenant_type in [tenant_type_1.lower(),
+                            tenant_type_2.lower(),
+                            tenant_type_3.lower()]:
+            self.assertIn(tenant_type,
+                          resp.json()[const.TENANT][const.NS_TYPES],
+                          msg="Not found {0}".format(tenant_type))
+
+    @ddt.file_data('data_invalid_tenant_types.json')
+    def test_create_tenant_with_invalid_tenant_types(self, test_data):
+        tenant_types = test_data.get('tenant_types')
+        error_message = test_data.get('error_message')
+        tenant_name = tenant_id = 'tname{0}'.format(
+            self.generate_random_string(const.LOWER_CASE_LETTERS))
+        request_object = requests.Tenant(
+                tenant_name=tenant_name,
+                description='A tenant described',
+                tenant_id=tenant_id,
+                enabled=True,
+                tenant_types=tenant_types,
+                display_name='A name displayed')
+        resp = self.identity_admin_client.add_tenant(tenant=request_object)
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(resp.json()[const.BAD_REQUEST][const.MESSAGE],
+                         error_message)
+
     @ddt.file_data('data_add_tenant.json')
-    def test_add_tenant(self, data_schema):
+    def test_add_tenant(self, test_data):
         '''Tests for add tenant API
 
         test_data comes from a json data file that can contain various possible
@@ -30,7 +83,7 @@ class TestAddTenant(base.TestBaseV2):
         @todo: The test_data file needs to be updated to include all possible
         data combinations.
         '''
-        test_data = data_schema.get('test_data', {})
+        test_data = test_data.get('test_data', {})
         tenant_name = (
             test_data.get('tenant_name', self.generate_random_string(
                           pattern=const.TENANT_NAME_PATTERN)))
@@ -51,10 +104,10 @@ class TestAddTenant(base.TestBaseV2):
         self.tenant_ids.append(tenant.id)
 
         add_tenant_schema = copy.deepcopy(tenants.add_tenant)
-        if 'additional_schema_fields' in data_schema:
+        if 'additional_schema_fields' in test_data:
             add_tenant_schema['properties']['tenant']['required'] = (
                 add_tenant_schema['properties']['tenant']['required'] +
-                data_schema['additional_schema_fields'])
+                test_data['additional_schema_fields'])
         self.assertSchema(response=resp, json_schema=add_tenant_schema)
 
         # The tenant_id passed in the request is ignored. The tenant_id is set
---
+++ tests/api/v2/tenants/test_misc_tenant_types_cases.py
@@ -0,0 +1,181 @@
+import ddt
+import copy
+from tests.api.v2 import base
+from tests.api.v2.schema import tenants
+from tests.api.v2.models import requests
+from tests.api.v2.models import factory
+from tests.api import constants as const
+
+
+@ddt.ddt
+class TestTenantTypes(base.TestBaseV2):
+    """
+    Test Tenant Types
+    """
+    def setUp(self):
+        super(TestTenantTypes, self).setUp()
+        self.tenant_ids = []
+        self.role_ids = []
+        self.user_ids = []
+        self.tenant_description = 'A tenant described'
+        self.tenant_display_name = 'A name displayed'
+        self.tenant_type_1 = self.generate_random_string(
+            pattern=const.TENANT_TYPE_PATTERN).upper()
+        self.tenant_type_2 = self.generate_random_string(
+            pattern=const.TENANT_TYPE_PATTERN).upper()
+        self.tenant_type_3 = self.generate_random_string(
+            pattern=const.TENANT_TYPE_PATTERN).upper()
+        self.add_tenant_schema = tenants.add_tenant
+        self.add_tenant_with_types_schema = copy.deepcopy(tenants.add_tenant)
+        (self.add_tenant_with_types_schema['properties'][const.TENANT]
+            ['properties'].update(
+                {const.NS_TYPES: {'type': 'array'}}))
+        (self.add_tenant_with_types_schema['properties'][const.TENANT]
+            ['required'].append(const.NS_TYPES))
+
+    def create_tenant_with_types(self):
+        tenant_name = tenant_id = 'tname{0}'.format(
+            self.generate_random_string(const.LOWER_CASE_LETTERS))
+        request_object = requests.Tenant(
+                tenant_name=tenant_name,
+                description=self.tenant_description,
+                tenant_id=tenant_id,
+                enabled=True,
+                tenant_types=[self.tenant_type_1,
+                              self.tenant_type_2,
+                              self.tenant_type_3],
+                display_name=self.tenant_display_name)
+        resp = self.identity_admin_client.add_tenant(tenant=request_object)
+        self.assertEqual(resp.status_code, 201)
+        self.tenant_ids.append(tenant_id)
+        self.assertSchema(response=resp,
+                          json_schema=self.add_tenant_with_types_schema)
+        self.assertEqual(len(
+            resp.json()[const.TENANT][const.NS_TYPES]), 3)
+        return tenant_id
+
+    def test_list_tenant_with_types(self):
+        tenant_name = self.create_tenant_with_types()
+        resp = self.identity_admin_client.get_tenant(tenant_id=tenant_name)
+        self.assertEqual(resp.status_code, 200)
+
+        uadm_username = "uadm_" + self.generate_random_string(
+            pattern=const.UPPER_CASE_LETTERS)
+        req_obj = factory.get_add_user_request_object_pull(
+            user_name=uadm_username)
+        resp = self.identity_admin_client.add_user(request_object=req_obj)
+        self.assertEqual(resp.status_code, 201)
+        new_user_id = resp.json()[const.USER][const.ID]
+        uadm_password = resp.json()[const.USER][const.NS_PASSWORD]
+        self.user_ids.append(new_user_id)
+
+        new_role_name = "NewRole"+self.generate_random_string(
+            pattern=const.UPPER_CASE_LETTERS)
+        req_obj = factory.get_add_role_request_object(
+            role_name=new_role_name,
+            administrator_role="identity:user-manage")
+        resp = self.identity_admin_client.add_role(request_object=req_obj)
+        self.assertEqual(resp.status_code, 201)
+        new_role_id = resp.json()[const.ROLE][const.ID]
+        self.role_ids.append(new_role_id)
+
+        resp = self.service_admin_client.add_role_to_user_for_tenant(
+                   tenant_id=tenant_name, role_id=new_role_id,
+                   user_id=new_user_id)
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.identity_admin_client.get_auth_token(
+                   user=uadm_username, password=uadm_password)
+        self.assertEqual(resp.status_code, 200)
+        uadm_token = resp.json()[const.ACCESS][const.TOKEN][const.ID]
+
+        resp = self.identity_admin_client.list_tenants(requestslib_kwargs={
+                   'headers': {'X-Auth-Token': uadm_token}})
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(len(resp.json()[const.TENANTS]) > 0,
+                        msg="Empty tenant list!")
+        target_tenant = None
+        for tenant in resp.json()[const.TENANTS]:
+            if tenant[const.NAME] == tenant_name:
+                target_tenant = tenant
+                break
+        self.assertIsNotNone(target_tenant, msg="Couldn't find tenant!")
+        self.assertIn(const.NS_TYPES, tenant)
+        self.assertEqual(len(target_tenant[const.NS_TYPES]), 3)
+
+    def test_list_tenants_with_types_by_domain(self):
+        tenant_id = tenant_name = self.create_tenant_with_types()
+        resp = self.identity_admin_client.get_tenant(tenant_id=tenant_id)
+        self.assertEqual(resp.status_code, 200)
+        domain_id = resp.json()[const.TENANT][const.RAX_AUTH_DOMAIN_ID]
+        resp = self.identity_admin_client.list_tenants_in_domain(
+                   domain_id=domain_id)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(len(resp.json()[const.TENANTS]) > 0,
+                        msg="Empty tenant list!")
+        target_tenant = None
+        for tenant in resp.json()[const.TENANTS]:
+            if tenant[const.NAME] == tenant_name:
+                target_tenant = tenant
+                break
+        self.assertIsNotNone(target_tenant, msg="Couldn't find tenant!")
+        self.assertIn(const.NS_TYPES, tenant)
+        self.assertEqual(len(target_tenant[const.NS_TYPES]), 3)
+
+    def test_delete_tenant_types_from_tenant(self):
+        tenant_id = tenant_name = self.create_tenant_with_types()
+        request_object = requests.Tenant(
+                tenant_name=tenant_name,
+                description=self.tenant_description,
+                tenant_id=tenant_id,
+                enabled=True,
+                tenant_types=[],
+                display_name=self.tenant_display_name)
+        resp = self.identity_admin_client.update_tenant(
+            tenant_id=tenant_id, request_object=request_object)
+        self.assertEqual(resp.status_code, 200)
+        self.assertSchema(response=resp,
+                          json_schema=self.add_tenant_schema)
+
+    # Negative duplicates ignored
+    def test_duplicate_tenant_types_create(self):
+        tenant_name = tenant_id = 'tname{0}'.format(
+            self.generate_random_string(const.LOWER_CASE_LETTERS))
+        request_object = requests.Tenant(
+                tenant_name=tenant_name,
+                description=self.tenant_description,
+                tenant_id=tenant_id,
+                enabled=True,
+                tenant_types=["B", "B", "A", "A", "C"],
+                display_name=self.tenant_display_name)
+        resp = self.identity_admin_client.add_tenant(tenant=request_object)
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(
+            ["a", "b", "c"],
+            sorted(resp.json()[const.TENANT][const.NS_TYPES]))
+
+    def test_duplicate_tenant_types_update(self):
+        tenant_id = tenant_name = self.create_tenant_with_types()
+        request_object = requests.Tenant(
+                tenant_name=tenant_name,
+                description=self.tenant_description,
+                tenant_id=tenant_id,
+                enabled=True,
+                tenant_types=["B", "B", "A", "A", "C"],
+                display_name=self.tenant_display_name)
+        resp = self.identity_admin_client.update_tenant(
+            tenant_id=tenant_id, request_object=request_object)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            ["a", "b", "c"],
+            sorted(resp.json()[const.TENANT][const.NS_TYPES]))
+
+    def tearDown(self):
+        for tenant_id in self.tenant_ids:
+            self.identity_admin_client.delete_tenant(tenant_id=tenant_id)
+        for user_id in self.user_ids:
+            self.identity_admin_client.delete_user(user_id=user_id)
+        for role_id in self.role_ids:
+            self.identity_admin_client.delete_role(role_id=role_id)
+
+        super(TestTenantTypes, self).tearDown()
