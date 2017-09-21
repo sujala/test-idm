93f5c901f49ecd8b29f2a17c710721b9bbed00c6
Bob Jacoby <robert.jacoby@rackspace.com>
2017-09-21 13:50:41 -0500

    [CID-1106] - Additional commit and nonCommit tests for adding role assignments (#1598)
    * [CID-1106] - Additional commit and nonCommit tests for adding role assignments to group
    
    - Adds test scenarios for empty strings in forTenants (both empty string in array, and empty string instead of array)
    - Adds non-commit test to demonstrate flow through adding roles and modifying them on a user group

--- src/nonCommitTest/groovy/com/rackspace/idm/modules/usergroups/api/resource/ManageUserGroupRolesRestIntegrationTest.groovy
+++ src/nonCommitTest/groovy/com/rackspace/idm/modules/usergroups/api/resource/ManageUserGroupRolesRestIntegrationTest.groovy
@@ -6,7 +6,7 @@ import com.rackspace.docs.identity.api.ext.rax_auth.v1.TenantAssignments
 import com.rackspace.docs.identity.api.ext.rax_auth.v1.UserGroup
 import com.rackspace.idm.Constants
 import com.rackspace.idm.domain.config.IdentityConfig
-import com.rackspace.idm.exception.ForbiddenException
+import org.apache.commons.collections4.CollectionUtils
 import org.apache.commons.lang.RandomStringUtils
 import org.apache.http.HttpStatus
 import org.openstack.docs.identity.api.v2.AuthenticateResponse
@@ -20,6 +20,8 @@ import testHelpers.IdmAssert
 import testHelpers.RootIntegrationTest
 
 import javax.ws.rs.core.MediaType
+import static Constants.ROLE_RBAC1_ID
+import static Constants.ROLE_RBAC2_ID
 
 class ManageUserGroupRolesRestIntegrationTest extends RootIntegrationTest {
 
@@ -49,8 +51,19 @@ class ManageUserGroupRolesRestIntegrationTest extends RootIntegrationTest {
         }
     }
 
+    /**
+     * Test a typical modification to the set of roles assign to a user group
+     *
+     * 1. No roles -> 1 tenant assigned role : Verify result includes that role assignment
+     * 2. Assign 1 global role : Verify both roles returned appropriately
+     * 3. Update tenant assigned role to be a global assigned role, and global to be assigned to 2 tenants: Verify result
+     * 4. Update tenant assigned role to only have single tenant; verify result as expected
+     * 5. Reset tenant to global : Verify result
+     *
+     * Test all this through both json and xml to verify can appriately reflect the states
+     */
     @Unroll
-    def "grant role to user group; mediaType = #mediaType"() {
+    def "modify roles on user group; mediaType = #mediaType"() {
         UserGroup group = new UserGroup().with {
             it.domainId = sharedUserAdmin.domainId
             it.name = "addRoleTest_" + RandomStringUtils.randomAlphanumeric(10)
@@ -58,47 +71,133 @@ class ManageUserGroupRolesRestIntegrationTest extends RootIntegrationTest {
         }
         def createdGroup = cloud20.createUserGroup(sharedIdentityAdminToken, group, mediaType).getEntity(UserGroup)
 
-        RoleAssignments assignments = new RoleAssignments().with {
+        RoleAssignments assignments0 = new RoleAssignments().with {
+            it.tenantAssignments = new TenantAssignments()
+            it
+        }
+
+        RoleAssignments assignments1 = new RoleAssignments().with {
+            TenantAssignments ta = new TenantAssignments()
+            ta.tenantAssignment.add(createTenantAssignment(ROLE_RBAC1_ID, [sharedUserAdminCloudTenant.id]))
+            it.tenantAssignments = ta
+            it
+        }
+
+        RoleAssignments assignments2 = new RoleAssignments().with {
             it.tenantAssignments = new TenantAssignments().with {
                 tas ->
-                    tas.tenantAssignment.add(new TenantAssignment().with {
-                       ta ->
-                           ta.onRole = Constants.ROLE_RBAC1_ID
-                           ta.forTenants.add("*")
-                           ta
-                    })
-                    tas.tenantAssignment.add(new TenantAssignment().with {
-                        ta ->
-                            ta.onRole = Constants.ROLE_RBAC2_ID
-                            ta.forTenants.add(sharedUserAdminCloudTenant.id)
-                            ta
-                    })
+                    tas.tenantAssignment.add(createTenantAssignment(ROLE_RBAC2_ID, ["*"]))
                     tas
             }
             it
         }
 
-        when:
-        def getResponse = cloud20.grantRoleAssignmentsOnUserGroup(sharedIdentityAdminToken, createdGroup, assignments, mediaType)
+        RoleAssignments assignments3 = new RoleAssignments().with {
+            it.tenantAssignments = new TenantAssignments().with {
+                tas ->
+                    tas.tenantAssignment.add(createTenantAssignment(ROLE_RBAC1_ID, ["*"]))
+                    tas.tenantAssignment.add(createTenantAssignment(ROLE_RBAC2_ID, [sharedUserAdminCloudTenant.id, sharedUserAdminFilesTenant.id]))
+                    tas
+            }
+            it
+        }
+
+        RoleAssignments assignments4 = new RoleAssignments().with {
+            it.tenantAssignments = new TenantAssignments().with {
+                tas ->
+                    tas.tenantAssignment.add(createTenantAssignment(ROLE_RBAC2_ID, [sharedUserAdminCloudTenant.id]))
+                    tas
+            }
+            it
+        }
+
+        RoleAssignments assignments5 = new RoleAssignments().with {
+            it.tenantAssignments = new TenantAssignments().with {
+                tas ->
+                    tas.tenantAssignment.add(createTenantAssignment(ROLE_RBAC2_ID, ["*"]))
+                    tas
+            }
+            it
+        }
+
+        when: "assignment 0"
+        def getResponse = cloud20.grantRoleAssignmentsOnUserGroup(sharedIdentityAdminToken, createdGroup, assignments0, mediaType)
+        RoleAssignments retrievedEntity = getResponse.getEntity(RoleAssignments)
 
         then:
         getResponse.status == HttpStatus.SC_OK
-        RoleAssignments retrievedEntity = getResponse.getEntity(RoleAssignments)
+        retrievedEntity.tenantAssignments == null
 
-        and:
+        when: "assignment 1"
+        getResponse = cloud20.grantRoleAssignmentsOnUserGroup(sharedIdentityAdminToken, createdGroup, assignments1, mediaType)
+        retrievedEntity = getResponse.getEntity(RoleAssignments)
+
+        then:
+        getResponse.status == HttpStatus.SC_OK
         retrievedEntity.tenantAssignments != null
-        def rbac1Assignment = retrievedEntity.tenantAssignments.tenantAssignment.find {it.onRole == Constants.ROLE_RBAC1_ID}
-        rbac1Assignment != null
-        rbac1Assignment.forTenants.size() == 1
-        rbac1Assignment.forTenants[0] == "*"
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC1_ID, [sharedUserAdminCloudTenant.id])
+        def rbac2Assignment = retrievedEntity.tenantAssignments.tenantAssignment.find {it.onRole == ROLE_RBAC2_ID}
+        rbac2Assignment == null
 
-        def rbac2Assignment = retrievedEntity.tenantAssignments.tenantAssignment.find {it.onRole == Constants.ROLE_RBAC2_ID}
-        rbac2Assignment != null
-        rbac2Assignment.forTenants.size() == 1
-        rbac2Assignment.forTenants[0] == sharedUserAdminCloudTenant.id
+        when: "assignment 2"
+        getResponse = cloud20.grantRoleAssignmentsOnUserGroup(sharedIdentityAdminToken, createdGroup, assignments2, mediaType)
+        retrievedEntity = getResponse.getEntity(RoleAssignments)
+
+        then:
+        getResponse.status == HttpStatus.SC_OK
+        retrievedEntity.tenantAssignments != null
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC1_ID, [sharedUserAdminCloudTenant.id])
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC2_ID, ["*"])
+
+        when: "assignment 3"
+        getResponse = cloud20.grantRoleAssignmentsOnUserGroup(sharedIdentityAdminToken, createdGroup, assignments3, mediaType)
+        retrievedEntity = getResponse.getEntity(RoleAssignments)
+
+        then:
+        getResponse.status == HttpStatus.SC_OK
+        retrievedEntity.tenantAssignments != null
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC1_ID, ["*"])
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC2_ID, [sharedUserAdminCloudTenant.id, sharedUserAdminFilesTenant.id])
+
+        when: "assignment 4"
+        getResponse = cloud20.grantRoleAssignmentsOnUserGroup(sharedIdentityAdminToken, createdGroup, assignments4, mediaType)
+        retrievedEntity = getResponse.getEntity(RoleAssignments)
+
+        then:
+        getResponse.status == HttpStatus.SC_OK
+        retrievedEntity.tenantAssignments != null
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC1_ID, ["*"])
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC2_ID, [sharedUserAdminCloudTenant.id])
+
+        when: "assignment 5"
+        getResponse = cloud20.grantRoleAssignmentsOnUserGroup(sharedIdentityAdminToken, createdGroup, assignments5, mediaType)
+        retrievedEntity = getResponse.getEntity(RoleAssignments)
+
+        then:
+        getResponse.status == HttpStatus.SC_OK
+        retrievedEntity.tenantAssignments != null
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC1_ID, ["*"])
+        verifyContainsAssignment(retrievedEntity, ROLE_RBAC2_ID, ["*"])
 
         where:
         mediaType << [MediaType.APPLICATION_XML_TYPE, MediaType.APPLICATION_JSON_TYPE]
+    }
+
+    void verifyContainsAssignment(RoleAssignments roleAssignments, String roleId, List<String> tenantIds) {
+        def rbac1Assignment = roleAssignments.tenantAssignments.tenantAssignment.find {it.onRole == roleId}
+        assert rbac1Assignment != null
+        assert rbac1Assignment.forTenants.size() == tenantIds.size()
+        assert CollectionUtils.isEqualCollection(rbac1Assignment.forTenants, tenantIds)
+    }
+
+    TenantAssignment createTenantAssignment(String roleId, List<String> tenants) {
+        def assignment = new TenantAssignment().with {
+            ta ->
+                ta.onRole = roleId
+                ta.forTenants.addAll(tenants)
+                ta
+        }
+        return assignment
     }
 
     @Unroll
