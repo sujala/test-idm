4ee99e0be5e22e2bd1a8496091e74308f80e9892
Dimitry Ushakov <dimitry.ushakov@rackspace.com>
2017-10-06 10:47:11 -0500

    CID-1113 Johny test to validate that user groups are removed for a user (#1644)
    * CID-1113 Johny test to validate that user groups are removed for a user

--- tests/api/v2/domain/test_list_users_in_user_group.py
+++ tests/api/v2/domain/test_list_users_in_user_group.py
@@ -3,7 +3,7 @@ import ddt
 
 from tests.api.v2.domain import usergroups
 from tests.api.v2.models import factory, responses
-from tests.api.v2.schema import users
+from tests.api.v2.schema import users, user_groups
 from tests.package.johny import constants as const
 
 
@@ -87,6 +87,80 @@ class ListUsersInUserGroup(usergroups.TestUserGroups):
         self.assertIn(
             self.user_manager_client.default_headers[const.X_USER_ID],
             user_id_list)
+
+    @ddt.data('user_admin', 'user_manager')
+    def test_get_users_in_user_group(self, user_type):
+
+        group_req = factory.get_add_user_group_request(self.domain_id)
+        create_group_resp = self.user_admin_client.add_user_group_to_domain(
+            domain_id=self.domain_id, request_object=group_req)
+        self.assertEqual(create_group_resp.status_code, 201)
+        group = responses.UserGroup(create_group_resp.json())
+
+        # adding both users to the user group
+        add_user_to_grp_resp = self.clients[user_type].add_user_to_user_group(
+            user_id=self.user_admin_client.default_headers[const.X_USER_ID],
+            group_id=group.id, domain_id=self.domain_id
+        )
+        self.assertEqual(add_user_to_grp_resp.status_code, 204)
+        add_user_to_grp_resp = self.clients[user_type].add_user_to_user_group(
+            user_id=self.user_manager_client.default_headers[const.X_USER_ID],
+            group_id=group.id, domain_id=self.domain_id
+        )
+        self.assertEqual(add_user_to_grp_resp.status_code, 204)
+
+        user_id = self.clients[user_type].default_headers[const.X_USER_ID]
+
+        # List user groups for user
+        list_groups_resp = (
+            self.clients[user_type].list_user_groups_for_domain(
+                domain_id=self.domain_id,
+                requestslib_kwargs={
+                    'params': {
+                        'userId': user_id
+                    }
+                }
+            ))
+        self.assertEqual(list_groups_resp.status_code, 200)
+        self.assertSchema(list_groups_resp,
+                          json_schema=user_groups.list_user_groups_for_domain)
+
+        user_group_id_list = [
+            user_group[const.ID] for user_group in list_groups_resp.json()[
+                const.RAX_AUTH_USER_GROUPS]]
+
+        self.assertIn(
+            group.id,
+            user_group_id_list)
+
+        # Delete users from user group
+        delete_users_resp = (
+            self.clients[user_type].delete_user_group_from_domain(
+                domain_id=self.domain_id, group_id=group.id))
+
+        self.assertEqual(delete_users_resp.status_code, 204)
+
+        # List user groups for user
+        list_groups_resp = (
+            self.clients[user_type].list_user_groups_for_domain(
+                domain_id=self.domain_id,
+                requestslib_kwargs={
+                    'params': {
+                        'userId': user_id
+                    }
+                }
+            ))
+        self.assertEqual(list_groups_resp.status_code, 200)
+        self.assertSchema(list_groups_resp,
+                          json_schema=user_groups.list_user_groups_for_domain)
+
+        user_group_id_list = [
+            user_group[const.ID] for user_group in list_groups_resp.json()[
+                const.RAX_AUTH_USER_GROUPS]]
+
+        self.assertNotIn(
+            group.id,
+            user_group_id_list)
 
     def tearDown(self):
         super(ListUsersInUserGroup, self).tearDown()
