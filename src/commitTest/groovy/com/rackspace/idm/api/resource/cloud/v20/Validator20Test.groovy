d7ac9cbcd25ac34600e9c769cb7e99fb8474bc2c
Bob Jacoby <robert.jacoby@rackspace.com>
2017-10-31 09:51:16 -0500

    [CID-1156] - Updated error message on dup IDP issuer creation (#1671)
    * [CID-1156] - Updated error message on dup IDP issuer creation
    
    * [CID-1156] - Updating comments for commit tests

--- src/commitTest/groovy/com/rackspace/idm/api/resource/cloud/v20/Validator20Test.groovy
+++ src/commitTest/groovy/com/rackspace/idm/api/resource/cloud/v20/Validator20Test.groovy
@@ -1,10 +1,14 @@
 package com.rackspace.idm.api.resource.cloud.v20
 
+import com.rackspace.docs.identity.api.ext.rax_auth.v1.IdentityProvider
+import com.rackspace.idm.ErrorCodes
 import com.rackspace.idm.domain.service.IdpPolicyFormatEnum
+import com.rackspace.idm.exception.DuplicateException
 import com.rackspace.idm.validation.JsonValidator
 import com.rackspace.idm.validation.Validator20
 import spock.lang.Shared
 import spock.lang.Unroll
+import testHelpers.IdmExceptionAssert
 import testHelpers.RootServiceTest
 
 class Validator20Test extends RootServiceTest {
@@ -21,6 +25,7 @@ class Validator20Test extends RootServiceTest {
         jsonValidator = Mock()
         service.jsonValidator = jsonValidator
         mockIdentityConfig(service)
+        mockFederatedIdentityService(service)
     }
 
     @Unroll
@@ -36,5 +41,38 @@ class Validator20Test extends RootServiceTest {
         policy                         | type   | jsonValidatorCalls
         '{"policy": {"name":"name"}"}' | "JSON" | 1
         "--- policy: name: name"       | "YAML" | 0
+    }
+
+    def "validateIdentityProviderIssuerWithDupCheck - if IDP already exists with issuer, throws Dup exception"() {
+        given:
+        IdentityProvider webProvider = new IdentityProvider().with {
+            it.issuer = "duplicate"
+            it
+        }
+        when:
+        service.validateIdentityProviderIssuerWithDupCheck(webProvider)
+
+        then: "calls to check dup"
+        1 * defaultFederatedIdentityService.getIdentityProviderByIssuer(webProvider.issuer) >> new com.rackspace.idm.domain.entity.IdentityProvider()
+
+        and: "throws dup exception when IDP already exists with that issuer"
+        Exception ex = thrown()
+        IdmExceptionAssert.assertException(ex, DuplicateException, ErrorCodes.ERROR_CODE_IDP_ISSUER_ALREADY_EXISTS, ErrorCodes.ERROR_CODE_IDP_ISSUER_ALREADY_EXISTS_MSG)
+    }
+
+    def "validateIdentityProviderIssuerWithDupCheck - if IDP does not exists with issuer, does not throw Dup exception"() {
+        given:
+        IdentityProvider webProvider = new IdentityProvider().with {
+            it.issuer = "duplicate"
+            it
+        }
+        when:
+        service.validateIdentityProviderIssuerWithDupCheck(webProvider)
+
+        then: "calls to check dup"
+        1 * defaultFederatedIdentityService.getIdentityProviderByIssuer(webProvider.issuer) >> null
+
+        and: "no error is thrown if dup check doesn't return an IDP"
+        notThrown()
     }
 }
