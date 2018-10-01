e3ff7c3c053f08b55fabab91e2648e1e877dbd51
Bob Jacoby <robert.jacoby@rackspace.com>
2018-10-01 16:05:21 -0500

    CID-1747: Removing additional resources (#2189)

--- scripts/coberturaCheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname $(readlink -f $0))
-cd $BASEDIR
-cd ..
-mvn cobertura:cobertura cobertura:check -Dskip.default.surefire=false -Dcobertura.skip.check=false -Didm.properties.location=target/config/JENKINS
