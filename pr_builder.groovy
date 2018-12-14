77769759350c1cf9ab6ee158e904f7f7ac3c59c0
Dimitry Ushakov <dimitry.ushakov@rackspace.com>
2018-12-14 11:24:25 -0600

    CID-1821: Rsi pr pipeline (#2244)
    * Add RSI pr pipeline

---
+++ pr_builder.groovy
@@ -0,0 +1,95 @@
+return [
+    version: 'v0.8.1',
+
+    unitTest: { scm ->
+        parallel(
+            test: {
+                node('java') {
+                    checkout scm
+                    try {
+                        sh './gradlew clean build commitCodeCoverage -x nonCommitTest --profile'
+                    } finally {
+                        junit allowEmptyResults: true, testResults: 'build/test-results/**/*.xml'
+                    }
+                }
+            },
+            publish: {
+                node('java') {
+                    checkout scm
+                    sh '''
+                    set +x
+                    source /tmp/secrets/secrets.sh
+                    echo artifactory_user=$ARTIFACTORY_USER >> gradle.properties
+                    echo artifactory_password=$ARTIFACTORY_PASSWORD >> gradle.properties
+                    set -x
+                    ./gradlew clean build -x test -x nonCommitTest -x commitTest -x commonTest artifactoryPublish --stacktrace
+                    '''
+                    env.IDM_Version = sh (
+                            script: './gradlew appRevision | head -2 | tail -1',
+                            returnStdout: true
+                    ).trim()
+                    println "idmVersion = ${env.IDM_VERSION}"
+                }
+            }
+        )
+        node('master') {
+            openshift.withCluster {
+                branches = [:]
+                ['ca-directory', 'customer-identity', 'repose', 'active-directory'].each {
+                    def name = it
+                    branches[name] = {
+                        def build = openshift.selector('bc', name).startBuild(
+                                "--commit=master", "--env=identity_version=${env.IDM_VERSION}", "--env=artifactory_host=https://artifacts.rackspace.net")
+                        waitUntil {
+                            return build.object().status.phase == 'Complete'
+                        }
+                        def digest = build.object().status.output.to.imageDigest
+                        openshift.tag("${name}@${digest}", "${name}:${env.ghprbSourceBranch}")
+                    }
+                }
+                timeout(15) {
+                    parallel branches
+                }
+            }
+        }
+    },
+
+    chart: [
+            name: 'customer-identity',
+            version: '0.1.2',
+            values: { branch, name ->
+                def deployConfig = [
+                        trigger_tag: branch,
+                        env: [
+                                values: [
+                                        [ name: 'http_proxy' ],
+                                        [ name: 'HTTP_PROXY' ],
+                                        [ name: 'LDAPS_PORT', value: 636],
+                                        [ name: 'TOMCAT_HTTP_PORT', value: 8083]
+                                ]
+                        ]
+                ]
+
+                return [
+                        'customer-identity-deploy': deployConfig,
+                        'repose-deploy': deployConfig,
+                        'active-directory-deploy': deployConfig,
+                        'ca-directory-deploy': deployConfig
+                ]
+            }
+    ],
+
+    functionalTest: { scm, name ->
+        node('test-functional') {
+            checkout scm
+            try {
+                tee('functional-test.log') {
+                    sh './run_johny_tests.sh'
+                }
+            } finally {
+                junit allowEmptyResults: true, testResults: '**/nosetests*.xml'
+                archiveArtifacts allowEmptyArchive: true, artifacts: 'functional-test.log'
+            }
+        }
+    }
+]
---
+++ run_johny_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+
+check_status() {
+    endpoint=$1
+    tries=$2
+    TEST_STATUS=`curl -so /dev/null -w '%{response_code}' $endpoint`
+    while [[ "$TEST_STATUS" != "200" ]]; do
+        echo "waiting for identity test cluster to set up ..."
+        echo "current status: $TEST_STATUS against $endpoint with try $tries"
+        sleep 10
+        tries=$((tries + 1))
+
+        if [[ $tries == 20 ]]; then
+            echo "FAILED TO START IDENTITY TEST CLUSTER"
+            exit 1
+        fi
+        TEST_STATUS=`curl -so /dev/null -w '%{response_code}' $endpoint`
+    done
+}
+cd tests
+TEST_CONFIG_DIR=/tmp/.identity
+mkdir -p $TEST_CONFIG_DIR
+mkdir -p $TEST_CONFIG_DIR/logs
+printenv
+pip install -r api/requirements.txt
+pip install opencafe
+pip install tox
+cafe-config init || true
+cafe-config plugins install http
+ls -latr
+TEST_ENDPOINT=http://${REPOSE_PORT_8080_TCP_ADDR}:${REPOSE_PORT_8080_TCP_PORT}
+check_status $TEST_ENDPOINT/idm/cloud
+TOKEN=`curl -s -X POST "${TEST_ENDPOINT}/idm/cloud/v2.0/tokens" -H 'content-type: application/json' -d "{ \"auth\": { \"passwordCredentials\":{ \"username\":\"AuthQE\", \"password\": \"Auth1234\" }}}" | python  -c 'import sys, json; print json.load(sys.stdin)["access"]["token"]["id"]'`
+cp etc/api.conf $TEST_CONFIG_DIR/api.conf
+sed -i -e "s|service_admin_auth_token=.*|service_admin_auth_token=${TOKEN}|g" $TEST_CONFIG_DIR/api.conf
+sed -i -e "s|base_url=.*|base_url=${TEST_ENDPOINT}|g" $TEST_CONFIG_DIR/api.conf
+sed -i -e "s|internal_url=.*|internal_url=${TEST_ENDPOINT}|g" $TEST_CONFIG_DIR/api.conf
+sed -i -e "s|nosetests --with-xunit --xunit-file=nosetests.xml|nosetests -a '!skip_at_gate' --with-xunit --xunit-file=nosetests.xml|g" tox.ini
+
+export CAFE_CONFIG_FILE_PATH=$TEST_CONFIG_DIR/api.conf
+export CAFE_ROOT_LOG_PATH=$TEST_CONFIG_DIR/logs
+export CAFE_TEST_LOG_PATH=$TEST_CONFIG_DIR/logs
+tox
