2366b483192d08354108699c95935d43d1e32120
Timothy Cline <timothy.cline@RACKSPACE.COM>
2019-03-04 10:21:43 -0600

    CID-1865 - Create Identity build pipeline and PR gate pipeline in RSI (#2348)
    * CID-1865 - Create Identity build pipeline and PR gate pipeline in RSI
    
    * Update to allow for installing sandbox into a specified namespace
    
    * Fixed johnny tests router name

--- pr_builder.groovy
+++ pr_builder.groovy
@@ -1,153 +1,61 @@
-return [
-    version: 'v0.8.1',
 
-    unitTest: { scm ->
-        parallel(
-            test: {
-                node('java') {
-                    checkout scm
-                    try {
-                        sh './gradlew clean build commitCodeCoverage -x nonCommitTest --profile'
-                    } finally {
-                        junit allowEmptyResults: true, testResults: 'build/test-results/**/*.xml'
-                    }
-                }
-            },
-            publish: {
-                node('java') {
-                    checkout scm
-                    sh '''
-                    set +x
-                    source /tmp/secrets/secrets.sh
-                    echo artifactory_user=$ARTIFACTORY_USER >> gradle.properties
-                    echo artifactory_password=$ARTIFACTORY_PASSWORD >> gradle.properties
-                    set -x
-                    ./gradlew clean build -x test -x nonCommitTest -x commitTest -x commonTest artifactoryPublish --stacktrace
-                    '''
-                    env.IDM_Version = sh (
-                            script: './gradlew appRevision | head -2 | tail -1',
-                            returnStdout: true
-                    ).trim()
-                    println "idmVersion = ${env.IDM_VERSION}"
-                }
-            }
-        )
-        node('master') {
-            openshift.withCluster {
-                branches = [:]
-                ['ca-directory', 'customer-identity', 'repose', 'active-directory', 'dynamodb'].each {
-                    def name = it
-                    branches[name] = {
-                        def build = openshift.selector('bc', name).startBuild(
-                                "--commit=master", "--env=identity_version=${env.IDM_VERSION}", "--env=artifactory_host=https://artifacts.rackspace.net")
-                        waitUntil {
-                            def status = build.object().status.phase
-                            println "build config ${name} is in status ${status}."
-                            return status == 'Complete'
-                        }
-                        def digest = build.object().status.output.to.imageDigest
-                        openshift.tag("${name}@${digest}", "${name}:${env.ghprbSourceBranch}")
-                    }
-                }
-                timeout(20) {
-                    parallel branches
-                }
+def namespace = 'identity-test'
+
+def build(scm) {
+
+    library "tesla@v0.8.2"
+
+    try {
+
+        // Setup the git configuration so it can be passed as an arg to common code
+        stage('Setup') {
+            node('master') {
+                cleanWs()
+                checkout scm
+                buildSteps = load('jenkins-scripts/jenkins-build-steps.groovy')
             }
         }
-    },
 
-    chart: [
-            name: 'customer-identity',
-            version: '0.1.4',
-            values: { branch, name ->
-                def dynamoDBConfig = [
-                        trigger_tag: branch,
-                        name: "dynamodb-$name",
-                        imageName: "dynamodb",
-                        env: [
-                                values: [
-                                        [ name: 'http_proxy' ],
-                                        [ name: 'HTTP_PROXY' ],
-                                        [ name: 'TOMCAT_HTTP_PORT', value: 8083]
-                                ]
-                        ]
-                ]
-                def custIdentityConfig = [
-                        trigger_tag: branch,
-                        name: "customer-identity-$name",
-                        imageName: "customer-identity",
-                        env: [
-                                values: [
-                                        [ name: 'http_proxy' ],
-                                        [ name: 'HTTP_PROXY' ],
-                                        [ name: 'TOMCAT_HTTP_PORT', value: 8083],
-                                        [ name: 'CA_DIRECTORY_SERVICE_HOST', value: "\$(CA_DIRECTORY_${name.toUpperCase().replaceAll('-','_')}_SERVICE_HOST)"],
-                                        [ name: 'CA_DIRECTORY_SERVICE_PORT', value: "\$(CA_DIRECTORY_${name.toUpperCase().replaceAll('-','_')}_SERVICE_PORT)"],
-                                        [ name: 'ACTIVE_DIRECTORY_SERVICE_HOST', value: "\$(ACTIVE_DIRECTORY_${name.toUpperCase().replaceAll('-','_')}_SERVICE_HOST)"],
-                                        [ name: 'ACTIVE_DIRECTORY_SERVICE_PORT', value: "\$(ACTIVE_DIRECTORY_${name.toUpperCase().replaceAll('-','_')}_SERVICE_PORT)"],
-                                        [ name: 'DYNAMODB_SERVICE_HOST', value: "\$(DYNAMODB_${name.toUpperCase().replaceAll('-','_')}_SERVICE_HOST):\$(DYNAMODB_${name.toUpperCase().replaceAll('-','_')}_SERVICE_PORT)"]
-                                ]
-                        ]
-                ]
-                def reposeConfig = [
-                        trigger_tag: branch,
-                        name: "repose-$name",
-                        imageName: "repose",
-                        env: [
-                                values: [
-                                        [ name: 'http_proxy' ],
-                                        [ name: 'HTTP_PROXY' ],
-                                        [ name: 'CUSTOMER_IDENTITY_SERVICE_HOST', value: "\$(CUSTOMER_IDENTITY_${name.toUpperCase().replaceAll('-','_')}_SERVICE_HOST)"],
-                                        [ name: 'CUSTOMER_IDENTITY_SERVICE_PORT', value: "\$(CUSTOMER_IDENTITY_${name.toUpperCase().replaceAll('-','_')}_SERVICE_PORT)"]
-                                ]
-                        ]
-                ]
-                def activeDirectoryConfig = [
-                        trigger_tag: branch,
-                        name: "active-directory-$name",
-                        imageName: "active-directory",
-                        env: [
-                                values: [
-                                        [ name: 'http_proxy' ],
-                                        [ name: 'HTTP_PROXY' ],
-                                        [ name: 'LDAPS_PORT', value: 636]
-                                ]
-                        ]
-                ]
-                def caDirectoryConfig = [
-                        trigger_tag: branch,
-                        name: "ca-directory-$name",
-                        imageName: "ca-directory",
-                        env: [
-                                values: [
-                                        [ name: 'http_proxy' ],
-                                        [ name: 'HTTP_PROXY' ],
-                                        [ name: 'LDAPS_PORT', value: 636]
-                                ]
-                        ]
-                ]
+        // Build and publish the artifact to test
+        env.IDM_VERSION = buildSteps.publishArtifact(scm)
+        env.NAMESPACE_NAME = namespace.toUpperCase().replaceAll('-', '_')
+        println "idmVersion = ${env.IDM_VERSION}"
 
-                return [
-                        'customer-identity-deploy': custIdentityConfig,
-                        'repose-deploy': reposeConfig,
-                        'active-directory-deploy': activeDirectoryConfig,
-                        'ca-directory-deploy': caDirectoryConfig,
-                        'dynamodb-deploy': dynamoDBConfig
-                ]
-            }
-    ],
 
-    functionalTest: { scm, name ->
-        node('test-functional') {
-            checkout scm
-            try {
-                tee('functional-test.log') {
-                    sh './run_johny_tests.sh'
-                }
-            } finally {
-                junit allowEmptyResults: true, testResults: '**/nosetests*.xml'
-                archiveArtifacts allowEmptyArchive: true, artifacts: 'functional-test.log'
+        // Build images for the sandbox environment
+        def sandboxImageTag = env.IDM_VERSION
+        buildSteps.buildImages(sandboxImageTag, env.IDM_VERSION, 'master')
+
+        def releaseName = null
+        try {
+            // Deploy sandbox env
+            env.SANDBOX_NAME = "pr-pipeline-jt-${env.BUILD_NUMBER}"
+            println "sandboxName = " + env.SANDBOX_NAME
+            releaseName = buildSteps.createStandboxEnv(sandboxImageTag, env.SANDBOX_NAME)
+            buildSteps.deploySandboxEnvironment(releaseName)
+
+            // Run Johnny tests
+            buildSteps.runJohnnyTests(scm)
+        } finally {
+            buildSteps.destroySandboxEnv(releaseName)
+        }
+
+    } catch (exc) {
+        throw exc
+    } finally {
+        node('master') {
+            cleanWs()
+        }
+        if (env.hasProperty('IDM_VERSION')) {
+            node('java') {
+                sh """
+            set +x
+            source /tmp/secrets/secrets.sh
+            curl -X DELETE https://artifacts.rackspace.net/artifactory/identity-maven-local/com/rackspace/idm/${env.IDM_VERSION}/ -H "X-JFrog-Art-Api: \${ARTIFACTORY_PASSWORD}" -v
+            """
             }
         }
     }
-]
+}
+
+return this
